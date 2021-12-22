a3a2883e9a45bf49ce7fd6c8672f889ccae262fa
utkarsh sharma <utkarsharma@utkarshs-MacBook-Pro.local>
2021-12-23 02:02:02 +0530

    Reverted dummy commit.

--- tests/operators/test_postgres_decorator.py
+++ tests/operators/test_postgres_decorator.py
@@ -253,8 +253,8 @@ class TestPostgresDecorator(unittest.TestCase):
         df = pd.read_sql(f"SELECT * FROM my_table", con=self.hook_target.get_conn())
         assert df.iloc[0].to_dict() == {
             "actor_id": 191,
-            "first_name": "FAIL",
-            "last_name": "FAIL",
+            "first_name": "GREGORY",
+            "last_name": "GOODING",
             "count": 30,
         }
 
