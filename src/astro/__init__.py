c55c70bb40d888ee2139275c655087962147facc
Daniel Imberman <daniel.imberman@gmail.com>
2022-01-24 16:20:33 -0800

    release-2.2

--- src/astro/__init__.py
+++ src/astro/__init__.py
@@ -16,7 +16,7 @@ limitations under the License.
 
 """A decorator that allows users to run SQL queries natively in Airflow."""
 
-__version__ = "0.1.1"
+__version__ = "0.2.2"
 from astro.dataframe import dataframe
 
 
