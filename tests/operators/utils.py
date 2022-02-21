67719701918344467c609080a7fb92293f5b16ba
utkarsh sharma <utkarsharma2@gmail.com>
2022-02-21 16:45:01 +0530

    Fixed load_file test case.

--- tests/operators/utils.py
+++ tests/operators/utils.py
@@ -28,7 +28,7 @@ SQL_SERVER_HOOK_PARAMETERS = {
 SQL_SERVER_CONNECTION_KEY = {
     "snowflake": "snowflake_conn_id",
     "postgres": "postgres_conn_id",
-    "bigquery": "bigquery_conn_id",
+    "bigquery": "gcp_conn_id",
 }
 
 SQL_SERVER_HOOK_CLASS = {
