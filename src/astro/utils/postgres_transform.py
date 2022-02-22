bfc44b0b141fc134fe0b08be81b5f3116e4e341a
utkarsh sharma <utkarsharma2@gmail.com>
2022-02-22 17:04:51 +0530

    Removed reference to provider packages directly.

--- src/astro/utils/postgres_transform.py
+++ src/astro/utils/postgres_transform.py
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from airflow.providers.postgres.hooks.postgres import PostgresHook
 from psycopg2.extensions import AsIs
 
 from astro.sql.table import Table
