3c743fdf91f6e7298de0a76da4060fcfb272b869
Daniel Imberman <daniel.imberman@gmail.com>
2022-02-28 08:46:14 -0800

    #143 render should not create a taskgroup

--- tests/parsers/test_sql_directory_parser.py
+++ tests/parsers/test_sql_directory_parser.py
@@ -51,9 +51,7 @@ class TestSQLParsing(unittest.TestCase):
             "join_customers_and_orders"
         ).operator.parameters
         assert len(join_params) == 2
-        assert (
-            join_params["customers_table"].operator.task_id == "render.customers_table"
-        )
+        assert join_params["customers_table"].operator.task_id == "customers_table"
 
     def test_parse_missing_table(self):
         with pytest.raises(AirflowException):
