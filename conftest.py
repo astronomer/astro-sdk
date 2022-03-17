15c9f24b09caa1f99df15833a42dfbca18008567
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2022-03-17 20:13:58 +0000

    Create utils.path module for path-related operations (#225)
    Relates to: #98

--- conftest.py
+++ conftest.py
@@ -1,25 +1,30 @@
 import os
+import pathlib
 import random
 import string
+import uuid
 
 import pytest
 import yaml
-from airflow.hooks.sqlite_hook import SqliteHook
+from airflow import settings
 from airflow.models import DAG, Connection, DagRun
 from airflow.models import TaskInstance as TI
+from airflow.providers.sqlite.hooks.sqlite import SqliteHook
 from airflow.utils import timezone
 from airflow.utils.db import create_default_connections
 from airflow.utils.session import create_session
 
 from astro.settings import SCHEMA
 from astro.sql.table import Table, TempTable
-from astro.utils.dependencies import BigQueryHook, PostgresHook, SnowflakeHook
+from astro.utils.cloud_storage_creds import parse_s3_env_var
+from astro.utils.dependencies import BigQueryHook, PostgresHook, SnowflakeHook, gcs, s3
 from tests.operators import utils as test_utils
 
 DEFAULT_DATE = timezone.datetime(2016, 1, 1)
 
 OUTPUT_TABLE_NAME = test_utils.get_table_name("integration_test_table")
 UNIQUE_HASH_SIZE = 16
+CWD = pathlib.Path(__file__).parent
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -112,3 +117,70 @@ def sql_server(request):
     yield (sql_name, hook)
     if not isinstance(hook, BigQueryHook):
         hook.run(f"DROP TABLE IF EXISTS {schema}.{OUTPUT_TABLE_NAME}")
+
+
+@pytest.fixture
+def remote_file(request):
+    param = request.param
+    provider = param["name"]
+    no_of_files = param["count"] if "count" in param else 1
+
+    if provider == "google":
+        conn_id = "test_google"
+        conn_type = "google_cloud_platform"
+        extra = {
+            "extra__google_cloud_platform__key_path": os.getenv(
+                "GOOGLE_APPLICATION_CREDENTIALS"
+            )
+        }
+    elif provider == "amazon":
+        key, secret = parse_s3_env_var()
+        conn_id = "test_amazon"
+        conn_type = "S3"
+        extra = {"aws_access_key_id": key, "aws_secret_access_key": secret}
+    else:
+        raise ValueError(f"File location {request.param} not supported")
+
+    new_connection = Connection(conn_id=conn_id, conn_type=conn_type, extra=extra)
+    session = settings.Session()
+    if not (
+        session.query(Connection)
+        .filter(Connection.conn_id == new_connection.conn_id)
+        .first()
+    ):
+        session.add(new_connection)
+        session.commit()
+
+    filename = pathlib.Path("tests/data/sample.csv")
+    object_paths = []
+    unique_value = uuid.uuid4()
+    for count in range(no_of_files):
+        object_prefix = f"test/{unique_value}__{count}.csv"
+        if provider == "google":
+            bucket_name = os.getenv("GOOGLE_BUCKET", "dag-authoring")
+            hook = gcs.GCSHook(gcp_conn_id=conn_id)
+            hook.upload(bucket_name, object_prefix, filename)
+            object_path = f"gs://{bucket_name}/{object_prefix}"
+        else:
+            bucket_name = os.getenv("AWS_BUCKET", "tmp9")
+            hook = s3.S3Hook(aws_conn_id=conn_id)
+            hook.load_file(filename, object_prefix, bucket_name)
+            object_path = f"s3://{bucket_name}/{object_prefix}"
+
+        object_paths.append(object_path)
+
+    yield conn_id, object_paths
+
+    for count in range(no_of_files):
+        object_prefix = f"test/{unique_value}__{count}.csv"
+        if provider == "google":
+            if hook.exists(bucket_name, object_prefix):
+                hook.delete(bucket_name, object_prefix)
+        else:
+            if hook.check_for_prefix(
+                object_prefix, delimiter="/", bucket_name=bucket_name
+            ):
+                hook.delete_objects(bucket_name, object_prefix)
+
+    session.delete(new_connection)
+    session.flush()
---
+++ src/astro/utils/path.py
@@ -0,0 +1,113 @@
+import glob
+import os
+import pathlib
+from urllib.parse import urlparse, urlunparse
+
+from astro.constants import FileLocation, FileType
+from astro.utils.cloud_storage_creds import gcs_client, s3fs_creds
+from astro.utils.dependencies import gcs, s3
+
+
+def get_location(path):
+    """
+    Identify where a file is located.
+
+    :param path: Path to a file in the filesystem
+    :type path: str
+    :return: Location of the file
+    :rtype: astro.constants.FileLocation (enum) constant
+    """
+    file_scheme = urlparse(path).scheme
+    if file_scheme == "":
+        location = FileLocation.LOCAL
+    else:
+        try:
+            location = getattr(FileLocation, file_scheme.upper())
+        except (UnboundLocalError, AttributeError):
+            raise ValueError(
+                f"Unsupported scheme '{file_scheme}' from path '{path}'"
+            )  # TODO: Use string interpolation as opposed to fstring
+    return location
+
+
+def validate_path(path):
+    """
+    Check if the give path is either a valid URI or a local file.
+
+    :param path: Either local filesystem path or remote URI
+    :type path: str
+    :return: if fails, raise `ValueError`
+    :rtype: `ValueError`
+    """
+    result = urlparse(path)
+    if not (all([result.scheme, result.netloc]) or os.path.isfile(path)):
+        raise ValueError(f"Invalid path: '{path}'")
+
+
+def get_transport_params(path, conn_id):
+    """
+    Given a filesystem, GCS or S3 prefix, return files within that path.
+
+    :param path: Either local filesystem path or remote URI
+    :param conn_id: Airflow connection ID, if connecting to S3 or GCS
+    :type path: str
+    :type conn_id: str
+    :return: A dictionary containing necessary credentials to access the file, to be used with open_smart
+    :rtype: Dict
+    """
+    location = get_location(path)
+    params = None
+    if location == FileLocation.S3:
+        params = s3fs_creds(conn_id)
+    elif location == FileLocation.GS:
+        params = gcs_client(conn_id)
+    return params
+
+
+def get_paths(path, conn_id=None):
+    """
+    Given a filesystem, GCS or S3 prefix, return files within that path.
+
+    :param path: Either local filesystem path or remote URI
+    :param conn_id: Airflow connection ID, if connecting to S3 or GCS
+    :type path: str
+    :type conn_id: str
+    :return: A list with URIs or local paths
+    :rtype: List(str)
+    """
+    url = urlparse(path)
+    location = get_location(path)
+    if location == FileLocation.LOCAL:
+        path = pathlib.Path(url.path)
+        if path.is_dir():
+            paths = [str(filepath) for filepath in path.rglob("*")]
+        else:
+            paths = glob.glob(url.path)
+    elif location in (FileLocation.HTTP, FileLocation.HTTPS):
+        paths = [path]
+    else:
+        bucket_name = url.netloc
+        prefix = url.path[1:]
+        if location == FileLocation.GS:
+            hook = gcs.GCSHook(gcp_conn_id=conn_id) if conn_id else gcs.GCSHook()
+            prefixes = hook.list(bucket_name=bucket_name, prefix=prefix)
+        else:  # location == FileLocation.S3:
+            hook = s3.S3Hook(aws_conn_id=conn_id) if conn_id else s3.S3Hook()
+            prefixes = hook.list_keys(bucket_name=bucket_name, prefix=prefix)
+        paths = [
+            urlunparse((url.scheme, url.netloc, keys, "", "", "")) for keys in prefixes
+        ]
+    return paths
+
+
+def is_local(path):
+    """
+    Check if a path is local to the filesystem or not.
+
+    :param path: Either a filesystem path or remote URI
+    :type path: str
+    :return: True or False
+    :rtype: bool
+    """
+    file_location = get_location(path)
+    return file_location == FileLocation.LOCAL
