54f772c50c35fa03dc391faec4cc9cfcdbaf3875
Utkarsh Sharma <utkarsharma2@gmail.com>
2022-04-21 13:43:30 +0530

    Fix: Support dataframes from different databases in dataframe operator.  (#325)
    Closes: #323
    
    Within method - _set_variables_from_first_table
    we try to fill in all DB params from the first table passes as a param to the data frame operator, there was a corner case not handled there.
    
    Check to see if all tables belong to the same conn_id. Otherwise, this can go wrong in cases
    
    When we have tables from different DBs.
    When we have tables from different conn_id, since they can be configured with different database/schema etc.

--- src/astro/utils/table_handler.py
+++ src/astro/utils/table_handler.py
@@ -16,28 +16,51 @@ class TableHandler:
         """
         first_table: Optional[Table] = None
         if self.op_args:
-            table_index = [x for x, t in enumerate(self.op_args) if type(t) == Table]
-            if table_index:
+            table_index = [
+                x for x, t in enumerate(self.op_args) if isinstance(t, Table)
+            ]
+            conn_id_set = {x.conn_id for x in self.op_args if isinstance(x, Table)}
+            # Check to see if all tables belong to same conn_id. Otherwise, we this can go wrong for cases
+            # 1. When we have tables from different DBs.
+            # 2. When we have tables from different conn_id, since they can be configured with different
+            # database/schema etc.
+            if table_index and len(conn_id_set) == 1:
                 first_table = self.op_args[table_index[0]]
-        elif not first_table:
+
+        if not first_table and self.op_kwargs and self.python_callable:
             table_kwargs = [
                 x
                 for x in inspect.signature(self.python_callable).parameters.values()
                 if (
                     x.annotation == Table
-                    and type(self.op_kwargs[x.name]) == Table
+                    and isinstance(self.op_kwargs[x.name], Table)
                     or x.annotation == pandas.DataFrame
-                    and type(self.op_kwargs[x.name]) == Table
+                    and isinstance(self.op_kwargs[x.name], Table)
                 )
             ]
-            if table_kwargs:
+            conn_id_set = {
+                self.op_kwargs[x.name].conn_id
+                for x in inspect.signature(self.python_callable).parameters.values()
+                if (
+                    x.annotation == Table
+                    and isinstance(self.op_kwargs[x.name], Table)
+                    or x.annotation == pandas.DataFrame
+                    and isinstance(self.op_kwargs[x.name], Table)
+                )
+            }
+            if table_kwargs and len(conn_id_set) == 1:
                 first_table = self.op_kwargs[table_kwargs[0].name]
 
         # If there is no first table via op_ags or kwargs, we check the parameters
-        elif not first_table:
+        if not first_table and self.parameters:
             if self.parameters:
-                param_tables = [t for t in self.parameters.values() if type(t) == Table]
-                if param_tables:
+                param_tables = [
+                    t for t in self.parameters.values() if isinstance(t, Table)
+                ]
+                conn_id_set = {
+                    t.conn_id for t in self.parameters.values() if isinstance(t, Table)
+                }
+                if param_tables and len(conn_id_set) == 1:
                     first_table = param_tables[0]
 
         if first_table:
