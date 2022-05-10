827dd67c69e8f2984ec648479ad4803459dc82dc
utkarsh sharma <utkarsharma2@gmail.com>
2022-05-10 14:05:17 +0530

    Removed redundant docstring comment.

--- src/astro/databases/base.py
+++ src/astro/databases/base.py
@@ -54,7 +54,6 @@ class BaseDatabase(ABC):
     def run_sql(self, sql_statement: str, parameters: Optional[dict] = None):
         """
         Return the results to running a SQL statement.
-        Return the results to running a SQL statement.
 
         Whenever possible, this method should be implemented using Airflow Hooks,
         since this will simplify the integration with Async operators.
