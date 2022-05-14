da925bd77e07221e0cb213e7e491e7eb233239ab
utkarsh sharma <utkarsharma2@gmail.com>
2022-05-14 16:57:36 +0530

    Fixed 'ttributeError: 'Table' object has no attribute 'drop’.' issue.

--- tests/operators/test_agnostic_boolean_check.py
+++ tests/operators/test_agnostic_boolean_check.py
@@ -4,101 +4,45 @@ Requires the unittest, pytest, and requests-mock Python libraries.
 """
 
 import logging
-import os
 import pathlib
 
 import pytest
 from airflow.exceptions import BackfillUnfinished
-from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from airflow.utils import timezone
 
 # Import Operator
 import astro.sql as aql
 from astro.constants import SUPPORTED_DATABASES
-from astro.settings import SCHEMA
 from astro.sql.operators.agnostic_boolean_check import Check
-from astro.sql.tables import Metadata, Table
-from tests.operators.utils import get_table_name, run_dag
+from astro.sql.tables import Table
+from tests.operators.utils import run_dag
 
 log = logging.getLogger(__name__)
 DEFAULT_DATE = timezone.datetime(2016, 1, 1)
 CWD = pathlib.Path(__file__).parent
 
 
-def drop_table_snowflake(
-    table_name: str,
-    conn_id: str = "snowflake_conn",
-    schema: str = os.environ["SNOWFLAKE_SCHEMA"],
-    database: str = os.environ["SNOWFLAKE_DATABASE"],
-    warehouse: str = os.environ["SNOWFLAKE_WAREHOUSE"],
-):
-    hook = SnowflakeHook(
-        snowflake_conn_id=conn_id,
-        schema=schema,
-        database=database,
-        warehouse=warehouse,
-    )
-    snowflake_conn = hook.get_conn()
-    cursor = snowflake_conn.cursor()
-    cursor.execute(f"DROP TABLE IF EXISTS {table_name} CASCADE;")
-    snowflake_conn.commit()
-    cursor.close()
-    snowflake_conn.close()
-
-
-@pytest.fixture(scope="module")
-def table(request):
-    boolean_check_table = Table(
-        name=get_table_name("boolean_check_test"),
-        metadata=Metadata(
-            database="pagila",
-            schema="airflow_test_dag",
-        ),
-        conn_id="postgres_conn",
-    )
-    boolean_check_table_bigquery = Table(
-        name=get_table_name("boolean_check_test"),
-        conn_id="bigquery",
-        metadata=Metadata(schema=SCHEMA),
-    )
-    boolean_check_table_sqlite = Table(
-        name=get_table_name("boolean_check_test"), conn_id="sqlite_conn"
-    )
-    boolean_check_table_snowflake = Table(
-        name=get_table_name("boolean_check_test"),
-        metadata=Metadata(
-            database=os.getenv("SNOWFLAKE_DATABASE"),  # type: ignore
-            schema=os.getenv("SNOWFLAKE_SCHEMA"),  # type: ignore
-            warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),  # type: ignore
-        ),
-        conn_id="snowflake_conn",
-    )
-    path = str(CWD) + "/../data/homes_append.csv"
-    tables = {
-        "postgres": boolean_check_table,
-        "bigquery": boolean_check_table_bigquery,
-        "sqlite": boolean_check_table_sqlite,
-        "snowflake": boolean_check_table_snowflake,
-    }
-
-    aql.load_file(
-        path=path,
-        output_table=tables[request.param],
-    ).operator.execute({"run_id": "foo"})
-
-    yield tables[request.param]
-
-    tables[request.param].drop()
-
-
-@pytest.mark.parametrize("table", SUPPORTED_DATABASES, indirect=True)
-def test_happyflow_success(sample_dag, table):
+@pytest.mark.integration
+@pytest.mark.parametrize("sql_server", SUPPORTED_DATABASES, indirect=True)
+@pytest.mark.parametrize(
+    "test_table",
+    [
+        {
+            "is_temp": True,
+            "path": str(CWD) + "/../data/homes_append.csv",
+            "load_table": True,
+        }
+    ],
+    indirect=True,
+    ids=["table"],
+)
+def test_happyflow_success(sample_dag, test_table):
     @aql.transform
     def get_table(input_table: Table):
         return "SELECT * FROM {{input_table}}"
 
     with sample_dag:
-        temp_table = get_table(table)
+        temp_table = get_table(test_table)
         aql.boolean_check(
             table=temp_table,
             checks=[Check("test_1", "rooms > 3")],
@@ -107,15 +51,28 @@ def test_happyflow_success(sample_dag, table):
     run_dag(sample_dag)
 
 
-@pytest.mark.parametrize("table", SUPPORTED_DATABASES, indirect=True)
-def test_happyflow_fail(sample_dag, table, caplog):
+@pytest.mark.integration
+@pytest.mark.parametrize("sql_server", SUPPORTED_DATABASES, indirect=True)
+@pytest.mark.parametrize(
+    "test_table",
+    [
+        {
+            "is_temp": True,
+            "path": str(CWD) + "/../data/homes_append.csv",
+            "load_table": True,
+        }
+    ],
+    indirect=True,
+    ids=["table"],
+)
+def test_happyflow_fail(sample_dag, test_table, caplog):
     @aql.transform
     def get_table(input_table: Table):
         return "SELECT * FROM {{input_table}}"
 
     with pytest.raises(BackfillUnfinished):
         with sample_dag:
-            temp_table = get_table(table)
+            temp_table = get_table(test_table)
             aql.boolean_check(
                 table=temp_table,
                 checks=[
@@ -129,8 +86,9 @@ def test_happyflow_fail(sample_dag, table, caplog):
     assert expected_error in caplog.text
 
 
+@pytest.mark.integration
 @pytest.mark.parametrize(
-    "table",
+    "sql_server",
     [
         "postgres",
         pytest.param(
@@ -149,13 +107,25 @@ def test_happyflow_fail(sample_dag, table, caplog):
     ],
     indirect=True,
 )
-def test_happyflow_success_with_templated_query(sample_dag, table):
+@pytest.mark.parametrize(
+    "test_table",
+    [
+        {
+            "is_temp": True,
+            "path": str(CWD) + "/../data/homes_append.csv",
+            "load_table": True,
+        }
+    ],
+    indirect=True,
+    ids=["table"],
+)
+def test_happyflow_success_with_templated_query(sample_dag, test_table):
     @aql.transform
     def get_table(input_table: Table):
         return "SELECT * FROM {{input_table}}"
 
     with sample_dag:
-        temp_table = get_table(table)
+        temp_table = get_table(test_table)
         aql.boolean_check(
             table=temp_table,
             checks=[Check("test_1", "{{table}}.rooms > 3")],
