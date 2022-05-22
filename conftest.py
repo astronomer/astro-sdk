40181dc1cd57e9aab30602ced6b4b96e5c4ea4e6
utkarsh sharma <utkarsharma2@gmail.com>
2022-05-22 15:47:49 +0530

    Fixed testcase.

--- conftest.py
+++ conftest.py
@@ -159,7 +159,6 @@ def table_fixture(request, sql_server):  # noqa: C901
                 "conn_id": hook.snowflake_conn_id,
                 "metadata": Metadata(
                     database=hook.database,
-                    warehouse=hook.warehouse,
                     schema=hook.schema,
                 ),
             }
