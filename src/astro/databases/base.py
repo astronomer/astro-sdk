544a119a55043a0fad3d20062fa10ca74b4ef4a6
utkarsh sharma <utkarsharma2@gmail.com>
2022-05-22 13:06:58 +0530

    Added comment to explain illegal_column_name_chars and illegal_column_name_chars_replacement in db class.

--- src/astro/databases/base.py
+++ src/astro/databases/base.py
@@ -35,6 +35,12 @@ class BaseDatabase(ABC):
     _drop_table_statement: str = "DROP TABLE IF EXISTS {}"
     _create_table_statement: str = "CREATE TABLE IF NOT EXISTS {} AS {}"
 
+    # Used to normalize the ndjson when appending fields in nested fields.
+    # Example -
+    #   ndjson - {'a': {'b': 'val'}}
+    #   the col names generated is 'a.b'. char '.' maybe an illegal char in some db's col name.
+    # Contains the illegal char and there replacement, where the value in
+    # illegal_column_name_chars[0] will be replaced by value in illegal_column_name_chars_replacement[0]
     illegal_column_name_chars: List[str] = []
     illegal_column_name_chars_replacement: List[str] = []
 
