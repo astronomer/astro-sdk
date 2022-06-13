251d55a95ab3bb16e14f667c61dad8dccc2dc57f
Daniel Imberman <daniel.imberman@gmail.com>
2022-06-13 16:32:15 -0700

    add default mode

--- src/astro/sql/operators/cleanup.py
+++ src/astro/sql/operators/cleanup.py
@@ -1,17 +1,35 @@
+import time
 from typing import List
 
 from airflow.decorators.base import get_unique_task_id
 from airflow.models.baseoperator import BaseOperator
+from airflow.utils.state import State
 
 from astro.databases import create_database
+from astro.sql.operators.base import BaseSQLOperator
+from astro.sql.operators.dataframe import DataframeOperator
 from astro.sql.table import Table
+
+
+def filter_for_temp_tables(tasks, context):
+    tables_to_clean = []
+    for task in tasks:
+        if isinstance(task, BaseSQLOperator) or isinstance(task, DataframeOperator):
+            task_output = task.output.resolve(context)
+            if isinstance(task_output, Table) and task_output.temp:
+                tables_to_clean.append(task_output)
+    return tables_to_clean
 
 
 class CleanupOperator(BaseOperator):
     """
-    Clean up temporary tables at the end of a DAG run
+    Clean up temporary tables at the end of a DAG run.
+
+    By default if no tables are
     :param tables_to_cleanup: List of tbles to drop at the end of the DAG run
     :param task_id: Optional custom task id
+    :param run_sync_mode: Whether to wait for the DAG to finish or not. Set to False if you want to immediately
+    clean all DAGs. Not that if you supply anything int `tables_to_cleanup` this argument is ignored.
     """
 
     template_fields = ("tables_to_cleanup",)
@@ -19,18 +37,81 @@ class CleanupOperator(BaseOperator):
     def __init__(
         self,
         *,
-        tables_to_cleanup: List[Table],
+        tables_to_cleanup: List[Table] = [],
         task_id: str = "",
+        run_sync_mode: bool = False,
         **kwargs,
     ):
         self.tables_to_cleanup = tables_to_cleanup
+        self.run_sync_mode = run_sync_mode
         task_id = task_id or get_unique_task_id("_cleanup")
 
         super().__init__(task_id=task_id, **kwargs)
 
     def execute(self, context: dict):
+        if not self.tables_to_cleanup:
+            if not self.run_sync_mode:
+                self.wait_for_dag_to_finish(context)
+            self.tables_to_cleanup = self.get_all_temp_tables(context)
         for table in self.tables_to_cleanup:
             if not isinstance(table, Table) or not table.temp:
                 continue
             db = create_database(table.conn_id)
+            self.log.info("Dropping table %s", table.name)
             db.drop_table(table)
+
+    def _is_dag_running(self, task_instances):
+        """
+        Given a list of task instances, determine whether the DAG (minus the current cleanup task) is still
+        running.
+
+        :param task_instances:
+        :return:
+        """
+        running_tasks = [
+            (ti.task_id, ti.state)
+            for ti in task_instances
+            if ti.task_id != self.task_id
+            and ti.state not in [State.SUCCESS, State.FAILED, State.SKIPPED]
+        ]
+        if running_tasks:
+            self.log.info(
+                "waiting on the following tasks to complete before cleaning up: %s",
+                running_tasks,
+            )
+            return True
+        else:
+            return False
+
+    def wait_for_dag_to_finish(self, context):
+        """
+        In the event that we are not given any tables, we will want to wait for all other tasks to finish before
+        we delete temporary tables. This prevents a scenario where either a) we delete temporary tables that
+        are still in use, or b) we run this function too early and then there are temporary tables that don't get
+        deleted.
+
+        Eventually this function should be made into an asynchronous function s.t. this operator does not take up a
+        worker slot.
+        :param context:
+        :return:
+        """
+
+        dag_is_running = True
+        current_dagrun = context["dag_run"]
+        while dag_is_running:
+            dag_is_running = self._is_dag_running(current_dagrun.get_task_instances())
+            if not dag_is_running:
+                time.sleep(5)
+
+    def get_all_temp_tables(self, context):
+        """
+        In the scenario where we are not given a list of tasks to follow, we will want to gather all temporary tables
+        To prevent scenarios where we grab objects that are not tables, we try to only follow up on SQL operators or
+        the dataframe operator, as these are the operators that return temporary tables.
+
+        :param context:
+        :return:
+        """
+        self.log.info("No tables provided, will delete all temporary tables")
+        tasks = [t for t in self.dag.tasks if t.task_id != self.task_id]
+        return filter_for_temp_tables(tasks=tasks, context=context)
--- tests/sql/operators/test_cleanup.py
+++ tests/sql/operators/test_cleanup.py
@@ -2,15 +2,24 @@ import pathlib
 
 import pandas
 import pytest
+from airflow.models.taskinstance import TaskInstance
+from airflow.operators.bash import BashOperator
+from airflow.utils.state import State
 
 import astro.sql as aql
 from astro.constants import Database
 from astro.files import File
+from astro.sql.operators.cleanup import CleanupOperator
 from astro.sql.table import Table
 
 CWD = pathlib.Path(__file__).parent
 
 DEFAULT_FILEPATH = str(pathlib.Path(CWD.parent.parent, "data/sample.csv").absolute())
+SQLITE_ONLY = [
+    {
+        "database": Database.SQLITE,
+    },
+]
 SUPPORTED_DATABASES = [
     {
         "database": Database.SQLITE,
@@ -144,3 +153,65 @@ def test_cleanup_multiple_table(database_table_fixture, tables_fixture):
     a.execute({})
     assert not db.table_exists(test_table_1)
     assert not db.table_exists(test_table_2)
+
+
+@pytest.mark.parametrize(
+    "database_table_fixture",
+    SQLITE_ONLY,
+    indirect=True,
+    # ids=["sqlite", "postgres", "bigquery", "snowflake"],
+)
+@pytest.mark.parametrize(
+    "tables_fixture",
+    [
+        {
+            "items": [
+                {
+                    "file": File(DEFAULT_FILEPATH),
+                },
+                {
+                    "file": File(DEFAULT_FILEPATH),
+                },
+            ]
+        }
+    ],
+    indirect=True,
+    ids=["two_tables"],
+)
+def test_cleanup_default_all_tables(sample_dag, database_table_fixture, tables_fixture):
+    from tests.sql.operators import utils as test_utils
+
+    @aql.transform
+    def foo(input_table: Table):
+        return "SELECT * FROM {{input_table}}"
+
+    table_1, table_2 = tables_fixture
+    with sample_dag:
+        foo(table_1)
+        foo(table_2)
+
+        aql.cleanup([])
+    test_utils.run_dag(sample_dag)
+
+
+def test_is_dag_running():
+    cleanup_op = CleanupOperator(task_id="cleanup")
+
+    task_instances = []
+    for i in range(4):
+        op = BashOperator(task_id=f"foo_task_{i}", bash_command="")
+        ti = TaskInstance(task=op, state=State.SUCCESS)
+        task_instances.append(ti)
+    assert not cleanup_op._is_dag_running(task_instances=task_instances)
+    task_instances[0].state = State.RUNNING
+    assert cleanup_op._is_dag_running(task_instances=task_instances)
+
+
+# def test_filter_for_temp_tables():
+#     cleanup_op = CleanupOperator(task_id="cleanup")
+#     task_instances = []
+#     for i in range(4):
+#         op = BashOperator(task_id=f"foo_task_{i}", bash_command="")
+#         ti = TaskInstance(task=op, state=State.SUCCESS)
+#         task_instances.append(ti)
+#     non_temp_task = Tr
