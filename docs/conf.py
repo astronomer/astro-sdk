e80c4c273fc77de21a38593933e790f9222189fb
Daniel Imberman <daniel.imberman@gmail.com>
2022-06-21 15:56:00 -0700

    try withouth autoapi

--- docs/conf.py
+++ docs/conf.py
@@ -32,9 +32,9 @@ release = "0.10.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["autoapi.extension"]
-autoapi_type = 'python'
-autoapi_dirs = ["../src"]
+# extensions = ["autoapi.extension"]
+# autoapi_type = 'python'
+# autoapi_dirs = ["../src"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
