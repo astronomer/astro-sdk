fa9b25a8835ccf9f6c80d98afb859fc53de7c033
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-06-23 22:00:11 +0300

    Check for optimised path between file location and database for transfer

--- src/astro/sql/operators/load_file.py
+++ src/astro/sql/operators/load_file.py
@@ -82,13 +82,19 @@ class LoadFile(BaseOperator):
             input_file.conn_id,
             normalize_config=self.normalize_config,
         ):
-            database.load_pandas_dataframe_to_table(
-                source_dataframe=file.export_to_dataframe(),
+            if not database.check_optimised_path_and_transfer(
+                source_file=file,
                 target_table=self.output_table,
-                if_exists=if_exists,
-                chunk_size=self.chunk_size,
-            )
-            if_exists = "append"
+            ):
+                return self.output_table
+            else:
+                database.load_pandas_dataframe_to_table(
+                    source_dataframe=file.export_to_dataframe(),
+                    target_table=self.output_table,
+                    if_exists=if_exists,
+                    chunk_size=self.chunk_size,
+                )
+                if_exists = "append"
         self.log.info("Completed loading the data into %s.", self.output_table)
         return self.output_table
 
