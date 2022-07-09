87571a1dc1127b5d7e6b9a990007fef9db330ee5
Utkarsh Sharma <utkarsharma2@gmail.com>
2022-07-10 03:35:02 +0530

    Update src/astro/settings.py
    Co-authored-by: Kaxil Naik <kaxilnaik@gmail.com>

--- src/astro/settings.py
+++ src/astro/settings.py
@@ -1,4 +1,4 @@
 from airflow.configuration import conf
 
 DEFAULT_SCHEMA = "tmp_astro"
-SCHEMA = conf.get("astro_sdk", "sql_schema", fallback=None) or DEFAULT_SCHEMA
+SCHEMA = conf.get("astro_sdk", "sql_schema", fallback=DEFAULT_SCHEMA)
