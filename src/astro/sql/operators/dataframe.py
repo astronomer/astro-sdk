b54daa20ded006ff13add8ac1c92d863c37a6562
Kaxil Naik <kaxilnaik@gmail.com>
2022-07-14 11:31:11 +0100

    Move config to store DF in XCom to settings file
    This way if we want to ever rename the config, we only need to do it at a single place and it is easy to find which configs users can control.
    
    At some point we should have a doc page that shows all the available configs

--- src/astro/sql/operators/dataframe.py
+++ src/astro/sql/operators/dataframe.py
@@ -2,12 +2,12 @@ import inspect
 from typing import Callable, Dict, Optional, Tuple, Union
 
 import pandas as pd
-from airflow.configuration import conf
 from airflow.decorators.base import DecoratedOperator
 
 from astro.constants import ColumnCapitalization
 from astro.databases import create_database
 from astro.exceptions import IllegalLoadToDatabaseException
+from astro.settings import ALLOW_UNSAFE_DF_STORAGE, IS_CUSTOM_XCOM_BACKEND
 from astro.sql.table import Table
 from astro.utils.dataframe import convert_columns_names_capitalization
 from astro.utils.table import find_first_table
@@ -149,10 +149,6 @@ class DataframeOperator(DecoratedOperator):
             )
             return self.output_table
         else:
-            if conf.get(
-                "core", "xcom_backend"
-            ) == "airflow.models.xcom.BaseXCom" and not conf.getboolean(
-                "astro_sdk", "dataframe_allow_unsafe_storage"
-            ):
+            if not IS_CUSTOM_XCOM_BACKEND and not ALLOW_UNSAFE_DF_STORAGE:
                 raise IllegalLoadToDatabaseException()
             return pandas_dataframe
--- src/astro/sql/operators/load_file.py
+++ src/astro/sql/operators/load_file.py
@@ -1,7 +1,6 @@
 from typing import Any, Dict, Optional, Union
 
 import pandas as pd
-from airflow.configuration import conf
 from airflow.models import BaseOperator
 from airflow.models.xcom_arg import XComArg
 
@@ -9,6 +8,7 @@ from astro.constants import DEFAULT_CHUNK_SIZE, ColumnCapitalization, LoadExistS
 from astro.databases import BaseDatabase, create_database
 from astro.exceptions import IllegalLoadToDatabaseException
 from astro.files import File, check_if_connection_exists, resolve_file_path_pattern
+from astro.settings import ALLOW_UNSAFE_DF_STORAGE, IS_CUSTOM_XCOM_BACKEND
 from astro.sql.table import Table
 from astro.utils.task_id_helper import get_task_id
 
@@ -74,11 +74,7 @@ class LoadFile(BaseOperator):
         if self.output_table:
             return self.load_data_to_table(input_file)
         else:
-            if conf.get(
-                "core", "xcom_backend"
-            ) == "airflow.models.xcom.BaseXCom" and not conf.getboolean(
-                "astro_sdk", "dataframe_allow_unsafe_storage"
-            ):
+            if not IS_CUSTOM_XCOM_BACKEND and not ALLOW_UNSAFE_DF_STORAGE:
                 raise IllegalLoadToDatabaseException()
             return self.load_data_to_dataframe(input_file)
 
