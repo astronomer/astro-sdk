89232288a3ddbfb02504d9d143ccd192d3b1d103
utkarsh sharma <utkarsharma2@gmail.com>
2022-07-25 19:01:27 +0530

    Release 1.0.0b1

--- src/astro/__init__.py
+++ src/astro/__init__.py
@@ -1,6 +1,6 @@
 """A decorator that allows users to run SQL queries natively in Airflow."""
 
-__version__ = "0.11.0"
+__version__ = "1.0.0b1"
 
 
 # The following line is an import work-around to avoid raising a circular dependency issue related to `create_database`
