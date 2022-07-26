06adb0350a5da851637538d4cbea98024dbdb936
utkarsh sharma <utkarsharma2@gmail.com>
2022-07-26 16:00:23 +0530

    Add check for pandas dataframe

--- src/astro/utils/dataframe.py
+++ src/astro/utils/dataframe.py
@@ -12,8 +12,9 @@ def convert_dataframe_col_case(
     :param df: dataframe whose cols will be altered
     :param columns_names_capitalization: String Literal with possible values - lower/Upper
     """
-    if columns_names_capitalization == "lower":
-        df.columns = [col_label.lower() for col_label in df.columns]
-    elif columns_names_capitalization == "upper":
-        df.columns = [col_label.upper() for col_label in df.columns]
+    if isinstance(df, pd.DataFrame):
+        if columns_names_capitalization == "lower":
+            df.columns = [col_label.lower() for col_label in df.columns]
+        elif columns_names_capitalization == "upper":
+            df.columns = [col_label.upper() for col_label in df.columns]
     return df
