90e54900d88b15cb6a5e4987e1bba2aaff0bba15
Daniel Imberman <daniel.imberman@gmail.com>
2022-08-11 12:13:57 -0700

    nit

--- src/astro/sql/operators/base_operator.py
+++ src/astro/sql/operators/base_operator.py
@@ -1,7 +1,11 @@
+from typing import Any
+
 from airflow.models.baseoperator import BaseOperator
+from airflow.utils.context import Context
 
 from astro.sql.operators.upstream_task_mixin import UpstreamTaskMixin
 
 
 class AstroSQLBaseOperator(UpstreamTaskMixin, BaseOperator):
-    pass
+    def execute(self, context: Context) -> Any:
+        pass
