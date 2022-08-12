9e5f731a4ad5b2abd018017a6322d15614f22eab
Pankaj <pankaj.singh@astronomer.io>
2022-08-12 17:19:41 +0530

    Example enable use_native_support

--- example_dags/example_dynamic_task_template.py
+++ example_dags/example_dynamic_task_template.py
@@ -29,7 +29,7 @@ with DAG(
             ),
             conn_id=ASTRO_GCP_CONN_ID,
         ),
-        use_native_support=False,
+        use_native_support=True,
     ).expand(input_file=get_file_list(path=GCS_BUCKET, conn_id=ASTRO_GCP_CONN_ID))
 
     # [END howto_operator_get_file_list]
