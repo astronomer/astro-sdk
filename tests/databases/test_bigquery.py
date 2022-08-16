d12f436bbcb08d32fe1d90686e6b4b53bb03ffe0
Utkarsh Sharma <utkarsharma2@gmail.com>
2022-08-16 13:00:28 +0530

    Update tests/databases/test_bigquery.py
    Co-authored-by: Felix Uellendall <feluelle@users.noreply.github.com>

--- tests/databases/test_bigquery.py
+++ tests/databases/test_bigquery.py
@@ -101,8 +101,8 @@ def test_bigquery_create_table_with_columns(database_table_fixture):
 
     # Looking for specific columns in INFORMATION_SCHEMA.COLUMNS as Bigquery can add/remove columns in the table.
     statement = (
-        f"SELECT TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, DATA_TYPE FROM {table.metadata.schema}"
-        f".INFORMATION_SCHEMA.COLUMNS WHERE table_name='{table.name}'"
+        f"SELECT TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, DATA_TYPE "
+        f"FROM {table.metadata.schema}.INFORMATION_SCHEMA.COLUMNS WHERE table_name='{table.name}'"
     )
     response = database.run_sql(statement)
     assert response.first() is None
