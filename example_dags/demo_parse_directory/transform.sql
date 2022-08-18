30951a24c8d51e1108a2ac2b75f02cd0b2dbcf5e
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-08-18 21:14:59 +0530

    Update docstring and example DAG for transform operator with SQL file (#667)
    * Update docstring and example DAG for transform_file with SQL file

---
+++ example_dags/demo_parse_directory/transform.sql
@@ -0,0 +1,1 @@
+SELECT Title, Rating FROM {{ input_table }} WHERE Genre1=='Animation' ORDER BY Rating desc LIMIT 5;
--- tests/test_example_dags.py
+++ tests/test_example_dags.py
@@ -61,6 +61,7 @@ def session():
         "example_load_file",
         "example_transform",
         "example_merge_bigquery",
+        "example_transform_file",
     ],
 )
 def test_example_dag(session, dag_id):
