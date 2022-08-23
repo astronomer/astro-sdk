17578454a3f3aaa0a9aece5b0391dc0d45138c61
Pankaj Koti <pankaj.koti@astronomer.io>
2022-08-23 11:22:08 +0530

    Add example Redshift load tasks for load_file example DAG

--- example_dags/example_load_file.py
+++ example_dags/example_load_file.py
@@ -151,4 +151,26 @@ with dag:
     )
     # [END load_file_example_12]
 
+    # [START load_file_example_13]
+    aql.load_file(
+        input_file=File(
+            "s3://astro-sdk/sample_pattern", conn_id="redshift_conn", filetype=FileType.CSV
+        ),
+        output_table=Table(conn_id="redshift_conn", metadata=Metadata(schema="astro")),
+        use_native_support=False,
+    )
+    # [END load_file_example_13]
+
+    # [START load_file_example_14]
+    aql.load_file(
+        input_file=File(
+            "gs://astro-sdk/workspace/sample_pattern",
+            conn_id="bigquery",
+            filetype=FileType.CSV,
+        ),
+        output_table=Table(conn_id="redshift_conn", metadata=Metadata(schema="astro")),
+        use_native_support=False,
+    )
+    # [END load_file_example_14]
+
     aql.cleanup()
