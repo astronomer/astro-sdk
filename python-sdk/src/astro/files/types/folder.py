faaaad2c6b92763ea41a80bf95b538245ee2e63d
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-09-04 03:03:41 +0545

    Add folders as filetype

---
+++ python-sdk/src/astro/files/types/folder.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import io
+
+import pandas as pd
+
+from astro.constants import FileType as FileTypeConstants
+from astro.files.types.base import FileType
+
+
+class FolderType(FileType):
+    """Concrete implementation to handle folder type"""
+
+    def export_to_dataframe(
+        self, stream, columns_names_capitalization="original", **kwargs
+    ):
+        raise NotImplementedError
+
+    def create_from_dataframe(self, df: pd.DataFrame, stream: io.TextIOWrapper) -> None:
+        raise NotImplementedError
+
+    @property
+    def name(self):
+        return FileTypeConstants.FOLDER
