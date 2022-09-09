05b62ac6ed0ca0e3a942e8741d89173367c05c0a
Phani Kumar <phani.kumar@astronomer.io>
2022-09-09 11:16:03 +0530

    Apply review suggestions

--- python-sdk/dev/dags/example_datasets.py
+++ python-sdk/dev/dags/example_datasets.py
@@ -1,21 +1,3 @@
-"""
-You can use Datasets(introduced in Airflow 2.4) to specify data dependencies in your DAGs.
-Note that in addition to scheduling DAGs based upon time,they can also be scheduled based
-upon a task updating a dataset.The below example showcases this functionality
-by specifying the Dataset dependency using a `schedule` parameter on the consumer DAG
-This Example DAG :
- - Pulls a CSV file from Github and loads it into a Postgres table, imdb_movies.
- - Once the load_file task(the producer DAG) has completed successfully,
-   Airflow schedules the transform_top_animations task(the consumer DAG) , since we
-   specified the Dataset dependency on imdb_movies_table using `schedule` parameter.
- - The transform_top_animations finds the Top 5 movies based on rating, and
-   loads it into another Postgres table, top_animation
-Pre-requisites:
- - Install Airflow 2.4
- - Install dependencies for Astro Python SDK with Postgres, refer to README.md
- - Create an Airflow Connection to connect to Postgres Table.
-"""
-
 from datetime import datetime
 
 from airflow import DAG
