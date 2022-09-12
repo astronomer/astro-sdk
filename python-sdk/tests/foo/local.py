19c8e7d447723aa14ee61dc16c7f36ae069c5e38
Daniel Imberman <danielimberman@Daniels-MBP.lan>
2022-09-12 11:28:19 -0700

    docs

--- python-sdk/tests/foo/local.py
+++ python-sdk/tests/foo/local.py
@@ -30,6 +30,20 @@ DEFAULT_DATE = timezone.datetime(2022, 1, 1)
 def local_dag_flow(
     subdir, dag_id, execution_date=timezone.utcnow(), session: Session = NEW_SESSION
 ):
+    """
+    Run a DAG locally without all of the extra airflow bits.
+
+    This function runs a DAG on your local machine without requiring an airflow scheduler or backfill job. Ulimately we
+    want to encourage a quick and iterative DAG writing process, so this system will only run tasks in a breadth-first
+    fashion and handle XCom, without all other Airflow logic.gi
+
+    Args:
+        subdir: directory or dag file we should parse. We recommend you parse the minimum number of files possible
+        dag_id: dag_id of the dag you'd like to run. Should exist within the subdir
+        execution_date: (optional) execution date of this DAG run
+    Returns:
+
+    """
     run_id = DagRun.generate_run_id(DagRunType.MANUAL, execution_date)
     dag = get_dag(subdir=subdir, dag_id=dag_id)
     dag.clear(dag_run_state=False)
@@ -77,7 +91,6 @@ def get_or_create_taskinstance(dr, run_id, session, task):
         .first()
     )
 
-# /    session.delete()
     if not ti:  # we should create the TI the first time we run it
         ti = TaskInstance(task, run_id=dr.run_id)
         ti.log.setLevel(logging.DEBUG)
@@ -105,6 +118,15 @@ def get_or_create_dagrun(dag, execution_date, run_id, session):
 
 
 def fix_keys(conn_dict):
+    """
+    Fixes a discrepancy where astronomer settings file stores values like host as `conn_host`, which is incompatible
+    with airflow Connection objects
+    Args:
+        conn_dict:
+
+    Returns:
+
+    """
     new_dict = {}
     for k, v in conn_dict.items():
         if k == "conn_id" or k == "conn_type":
@@ -115,6 +137,15 @@ def fix_keys(conn_dict):
 
 
 def create_database_connections(settings_file):
+    """
+    Upload the settings in a file to your database as connections. This script assumes that you are using
+    a throwaway sqlite instance, please be careful when using persistent databases!
+    Args:
+        settings_file: Path to the airflow_settings.yaml file
+
+    Returns:
+
+    """
     with open(settings_file) as fp:
         yaml_with_env = os.path.expandvars(fp.read())
         yaml_dicts = yaml.safe_load(yaml_with_env)
@@ -122,6 +153,7 @@ def create_database_connections(settings_file):
         for i in yaml_dicts['airflow']["connections"]:
             i = fix_keys(i)
             connections.append(Connection(**i))
+    # Add connections to DB
     with create_session() as session:
         session.query(DagRun).delete()
         session.query(TI).delete()
