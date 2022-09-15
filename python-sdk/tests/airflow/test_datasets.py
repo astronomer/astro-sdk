43c922bdedffbdb67a5f5a43c7d241bf4a5cf928
Phani Kumar <94376113+phanikumv@users.noreply.github.com>
2022-09-15 20:08:06 +0530

    Add `example_datasets.py` DAG to unit tests (#839)

--- python-sdk/tests/airflow/test_datasets.py
+++ python-sdk/tests/airflow/test_datasets.py
@@ -1,6 +1,9 @@
+import os
 from unittest import mock
 
+import airflow
 import pytest
+from airflow.models.dagbag import DagBag
 from astro.airflow.datasets import kwargs_with_datasets
 from astro.sql.table import Table
 
@@ -76,3 +79,22 @@ def test_kwargs_with_datasets(
             kwargs_with_datasets(kwargs, input_datasets, output_datasets)
             == expected_kwargs
         )
+
+
+@pytest.mark.skipif(
+    airflow.__version__ < "2.4.0", reason="Require Airflow version >= 2.4.0"
+)
+def test_example_dataset_dag():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    db = DagBag(dir_path + "/../../example_dags")
+
+    from airflow.datasets import Dataset
+
+    producer_dag = db.get_dag("example_dataset_producer")
+    consumer_dag = db.get_dag("example_dataset_consumer")
+    # Test that last task in the producer DAG produces an outlet
+    outlets = producer_dag.tasks[-1].outlets
+    assert isinstance(outlets[0], Dataset)
+    # Test that dataset_triggers is only set if all the instances passed to the DAG object are Datasets
+    assert consumer_dag.dataset_triggers == outlets
+    assert outlets[0].uri == "astro://sqlite_default@?table=imdb_movies"
