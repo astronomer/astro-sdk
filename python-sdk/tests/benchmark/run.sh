7bb0fd2c469ef891f709f45c2164c0866ff58429
Pankaj <pankaj.singh@astronomer.io>
2022-09-26 17:54:36 +0530

    cleanup

--- python-sdk/tests/benchmark/run.sh
+++ python-sdk/tests/benchmark/run.sh
@@ -19,12 +19,6 @@ if test -f "${benchmark_dir}/test-connections.yaml"; then
 else
   connections_file="${benchmark_dir}/../../test-connections.yaml"
 fi
-ls -al
-echo "===================================================="
-cat $connections_file
-
-
-echo "===================================================="
 
 git_revision="${GIT_HASH:=`git rev-parse --short HEAD`}"
 echo $git_revision
