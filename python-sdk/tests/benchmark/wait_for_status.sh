c1cb1125c14503d0650a5a43580b2134a14b277f
Pankaj <pankaj.singh@astronomer.io>
2022-09-27 18:14:59 +0530

    test

--- python-sdk/tests/benchmark/wait_for_status.sh
+++ python-sdk/tests/benchmark/wait_for_status.sh
@@ -1,23 +1,16 @@
-# wait for completion as background process - capture PID
-kubectl wait --for=condition=complete job/benchmark -n benchmark &
-completion_pid=$!
-
-# wait for failure as background process - capture PID
-kubectl wait --for=condition=failed job/benchmark -n benchmark && exit 1 &
-failure_pid=$!
-
-# capture exit code of the first subprocess to exit
-wait -n $completion_pid $failure_pid
-
-# store exit code in variable
-exit_code=$?
-
-if (( $exit_code == 0 )); then
-  echo "Job completed"
-else
-  echo "Job failed with exit code ${exit_code}, exiting..."
-fi
+retval_complete=1
+retval_failed=1
+while [[ $retval_complete -ne 0 ]] && [[ $retval_failed -ne 0 ]]; do
+  sleep 5
+  output=$(kubectl wait --for=condition=failed job/benchmark -n benchmark --timeout=0 2>&1)
+  retval_failed=$?
+  output=$(kubectl wait --for=condition=complete job/benchmark -n benchmark --timeout=0 2>&1)
+  retval_complete=$?
+done
 
 kubectl delete job benchmark -n benchmark
 
-exit $exit_code
+if [ $retval_failed -eq 0 ]; then
+    echo "Job failed. Please check logs."
+    exit 1
+fi
