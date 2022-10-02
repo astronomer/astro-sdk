b195480addb66a2f4eb8d1fd6db22260399dcfe1
Utkarsh Sharma <utkarsharma2@gmail.com>
2022-10-02 21:25:49 +0530

    Publish results (#976)
    Publish results

--- python-sdk/tests/benchmark/settings.py
+++ python-sdk/tests/benchmark/settings.py
@@ -6,7 +6,7 @@ publish_benchmarks_db_conn_id = os.getenv(
 )
 publish_benchmarks_schema = os.getenv("ASTRO_PUBLISH_BENCHMARK_SCHEMA", "benchmark")
 publish_benchmarks_table = os.getenv(
-    "ASTRO_PUBLISH_BENCHMARK_TABLE", "load_files_to_database"
+    "ASTRO_PUBLISH_BENCHMARK_TABLE", "load_files_to_databaseV2"
 )
 publish_benchmarks_table_grouping_col = os.getenv(
     "ASTRO_PUBLISH_BENCHMARK_GROUPING_COL", "revision"
