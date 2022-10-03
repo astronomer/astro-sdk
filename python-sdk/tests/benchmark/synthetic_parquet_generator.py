ae3ff922c2e4632287fdede5e5734e710a82510f
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-10-03 13:49:20 +0530

    Remove local path

--- python-sdk/tests/benchmark/synthetic_parquet_generator.py
+++ python-sdk/tests/benchmark/synthetic_parquet_generator.py
@@ -28,29 +28,29 @@ def generate_data(file_name, start, end):
     print(f"File {file_name} has been created.")
 
 
-five_gb = "/home/ankit_chaurasia/fake_dataset/parquet/five_gb.parquet"
+five_gb = "/fake_dataset/parquet/five_gb.parquet"
 five_gb_rows = 8849701
 generate_data(five_gb, 0, five_gb_rows)
 #
 
 # create one_gb parquet file
-one_gb = "/home/ankit_chaurasia/fake_dataset/parquet/ten_gb/sixth.parquet"
+one_gb = "/fake_dataset/parquet/ten_gb/sixth.parquet"
 one_gb_rows = 9697180
 generate_data(one_gb, 8080983, one_gb_rows)
 
 # create ten gb parquet files
-one_gb = "/home/ankit_chaurasia/fake_dataset/parquet/ten_gb/seventh.parquet"
+one_gb = "/fake_dataset/parquet/ten_gb/seventh.parquet"
 one_gb_rows = 11313377
 generate_data(one_gb, 9697182, one_gb_rows)
 
-two_gb = "/home/ankit_chaurasia/fake_dataset/parquet/ten_gb/eighth.parquet"
+two_gb = "/fake_dataset/parquet/ten_gb/eighth.parquet"
 two_gb_rows = 12929574
 generate_data(two_gb, 11313379, two_gb_rows)
 
-two_gb = "/home/ankit_chaurasia/fake_dataset/parquet/ten_gb/ninth.parquet"
+two_gb = "/fake_dataset/parquet/ten_gb/ninth.parquet"
 two_gb_rows = 14545771
 generate_data(two_gb, 12929576, two_gb_rows)
 
-two_gb = "/home/ankit_chaurasia/fake_dataset/parquet/ten_gb/tenth.parquet"
+two_gb = "/fake_dataset/parquet/ten_gb/tenth.parquet"
 two_gb_rows = 16161968
 generate_data(two_gb, 14545773, two_gb_rows)
