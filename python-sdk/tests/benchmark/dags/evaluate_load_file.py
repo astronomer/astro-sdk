57789806afd0b90c0bb2c5b65599ccbb0982487a
Utkarsh Sharma <utkarsharma2@gmail.com>
2022-10-03 15:28:20 +0530

    Add kwargs space in config for database/dataset combo (#974)
    # Description
    ## What is the current behavior?
    We need a space where we can pass the kwargs specif to a
    database/dataset combination:
    if we have a dataset:
    
    `s3://astro-sdk/benchmark/trimmed/covid_overview/covid_overview_10kb.csv`
    
    We need different kwargs passed to native method for different db
    1. Redshift
    ```
    {
             native_support_kwargs: {
                "IGNOREHEADER": 1,
                 "REGION": "us-west-2",
                 "IAM_ROLE": REDSHIFT_NATIVE_LOAD_IAM_ROLE_ARN".
             },
             "enable_native_fallback": false
        }
    ```
    
    2. Bigquery
    
    ```
    {
           native_support_kwargs: {
                 "skip_leading_rows": 1,
           },
          "enable_native_fallback": false
    }
    ```
    related: #972
    
    ## What is the new behavior?
    Added a field in config to have this db/dataset-specific config.
    
    ## Does this introduce a breaking change?
    Nope
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- python-sdk/tests/benchmark/dags/evaluate_load_file.py
+++ python-sdk/tests/benchmark/dags/evaluate_load_file.py
@@ -43,13 +43,82 @@ def get_location(path):
     return scheme
 
 
-def create_dag(database_name, table_args, dataset, kwargs):
+def create_dag(database_name, table_args, dataset, global_db_kwargs):
+    """
+    Create dag dynamically
+
+    Override params from config
+    example config:
+    {
+        "databases": [
+            {
+                "kwargs": {                            # global config bigquery db
+                    "enable_native_fallback": false
+                },
+                "name": "biquery",
+                "output_table": {
+                    "conn_id": "biquery"
+                }
+            },{
+                 "kwargs": {                          # global config for redshift db
+                      "enable_native_fallback": false
+                  },
+                  "name": "redshift",
+                  "output_table": {
+                      "conn_id": "redshift"
+                  }
+            }],
+          "datasets": [
+                {
+                    "conn_id": "aws",
+                    "file_type": "parquet",
+                    "name": "ten_kb",
+                    "path": "s3://astro-sdk/benchmark/trimmed/covid_overview/covid_overview_10kb.csv",
+                    "rows": 160,
+                    "size": "10 KB",
+                    "database_kwargs" : {
+                         "biquery": {                           # local config for bigquery db
+                             native_support_kwargs: {"skip_leading_rows": 1}
+                         },
+                         "redshift": {                          # local config for redshift db
+                               native_support_kwargs:{
+                                  "IGNOREHEADER": 1,
+                                  "REGION": "us-west-2",
+                                  "IAM_ROLE": "REDSHIFT_NATIVE_LOAD_IAM_ROLE_ARN",
+                                }
+                         }
+                     }
+                }]
+    }
+
+    Final config for load_file()
+        1. Redshift
+            {
+                native_support_kwargs: {
+                    "IGNOREHEADER": 1,
+                    "REGION": "us-west-2",
+                    "IAM_ROLE": REDSHIFT_NATIVE_LOAD_IAM_ROLE_ARN".
+                },
+                "enable_native_fallback": false
+            }
+        2.  Bigquery
+            {
+                native_support_kwargs: {
+                    "skip_leading_rows": 1,
+                },
+                "enable_native_fallback": false
+            }
+    Note - If there is a config present in both local and global config.
+    Local config will override the global one."""
+
     dataset_name = dataset["name"]
     dataset_path = dataset["path"]
     dataset_conn_id = dataset.get("conn_id")
     dataset_filetype = dataset.get("file_type")
     location = get_location(dataset_path)
     # dataset_rows = dataset["rows"]
+    dataset_databases_kwargs = dataset.get("database_kwargs", {})
+    local_db_kwargs = dataset_databases_kwargs.get(database_name, {})
 
     dag_name = (
         f"load_file_{dataset_name}_{dataset_filetype}_{location}_into_{database_name}"
@@ -73,9 +142,9 @@ def create_dag(database_name, table_args, dataset, kwargs):
             "output_table": table,
             "chunk_size": chunk_size,
         }
-        # Override params from config
-        params.update(kwargs)
 
+        params.update(global_db_kwargs)
+        params.update(local_db_kwargs)
         my_table = aql.load_file(**params)
         aql.cleanup([my_table])
 
@@ -94,9 +163,9 @@ config = load_config()
 for database in config["databases"]:
     database_name = database["name"]
     table_args = database["output_table"]
-    kwargs = database["kwargs"]
+    global_db_kwargs = database["kwargs"]
     for dataset in config["datasets"]:
         table_args_copy = table_args.copy()
 
-        dag = create_dag(database_name, table_args_copy, dataset, kwargs)
+        dag = create_dag(database_name, table_args_copy, dataset, global_db_kwargs)
         globals()[dag.dag_id] = dag
