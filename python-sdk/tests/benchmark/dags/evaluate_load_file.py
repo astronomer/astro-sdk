0e6515ec32dd58dcdd26c1daed80a9c03413ba09
utkarsh sharma <utkarsharma2@gmail.com>
2022-10-03 15:24:51 +0530

    Moved comment to docstring

--- python-sdk/tests/benchmark/dags/evaluate_load_file.py
+++ python-sdk/tests/benchmark/dags/evaluate_load_file.py
@@ -5,10 +5,11 @@ from pathlib import Path
 from urllib.parse import urlparse
 
 from airflow import DAG
-from astro import sql as aql
 from astro.constants import DEFAULT_CHUNK_SIZE, FileType
-from astro.files import File
 from astro.sql.table import Metadata, Table
+
+from astro import sql as aql
+from astro.files import File
 
 START_DATE = datetime(2000, 1, 1)
 
@@ -44,6 +45,73 @@ def get_location(path):
 
 
 def create_dag(database_name, table_args, dataset, global_db_kwargs):
+    """
+    Create dag dynamically
+
+    Override params from config
+    example config:
+    {
+        "databases": [
+            {
+                "kwargs": {                            # global config bigquery db
+                    "enable_native_fallback": false
+                },
+                "name": "biquery",
+                "output_table": {
+                    "conn_id": "biquery"
+                }
+            },{
+                 "kwargs": {                          # global config for redshift db
+                      "enable_native_fallback": false
+                  },
+                  "name": "redshift",
+                  "output_table": {
+                      "conn_id": "redshift"
+                  }
+            }],
+          "datasets": [
+                {
+                    "conn_id": "aws",
+                    "file_type": "parquet",
+                    "name": "ten_kb",
+                    "path": "s3://astro-sdk/benchmark/trimmed/covid_overview/covid_overview_10kb.csv",
+                    "rows": 160,
+                    "size": "10 KB",
+                    "database_kwargs" : {
+                         "biquery": {                           # local config for bigquery db
+                             native_support_kwargs: {"skip_leading_rows": 1}
+                         },
+                         "redshift": {                          # local config for redshift db
+                               native_support_kwargs:{
+                                  "IGNOREHEADER": 1,
+                                  "REGION": "us-west-2",
+                                  "IAM_ROLE": "REDSHIFT_NATIVE_LOAD_IAM_ROLE_ARN",
+                                }
+                         }
+                     }
+                }]
+    }
+
+    Final config for load_file()
+        1. Redshift
+            {
+                native_support_kwargs: {
+                    "IGNOREHEADER": 1,
+                    "REGION": "us-west-2",
+                    "IAM_ROLE": REDSHIFT_NATIVE_LOAD_IAM_ROLE_ARN".
+                },
+                "enable_native_fallback": false
+            }
+        2.  Bigquery
+            {
+                native_support_kwargs: {
+                    "skip_leading_rows": 1,
+                },
+                "enable_native_fallback": false
+            }
+    Note - If there is a config present in both local and global config.
+    Local config will override the global one."""
+
     dataset_name = dataset["name"]
     dataset_path = dataset["path"]
     dataset_conn_id = dataset.get("conn_id")
@@ -76,73 +144,8 @@ def create_dag(database_name, table_args, dataset, global_db_kwargs):
             "chunk_size": chunk_size,
         }
 
-        # Override params from config
-        # example config:
-        # {
-        #     "databases": [
-        #         {
-        #             "kwargs": {                            # global config bigquery db
-        #                 "enable_native_fallback": false
-        #             },
-        #             "name": "biquery",
-        #             "output_table": {
-        #                 "conn_id": "biquery"
-        #             }
-        #         },{
-        #              "kwargs": {                          # global config for redshift db
-        #                   "enable_native_fallback": false
-        #               },
-        #               "name": "redshift",
-        #               "output_table": {
-        #                   "conn_id": "redshift"
-        #               }
-        #         }],
-        #       "datasets": [
-        #             {
-        #                 "conn_id": "aws",
-        #                 "file_type": "parquet",
-        #                 "name": "ten_kb",
-        #                 "path": "s3://astro-sdk/benchmark/trimmed/covid_overview/covid_overview_10kb.csv",
-        #                 "rows": 160,
-        #                 "size": "10 KB",
-        #                 "database_kwargs" : {
-        #                      "biquery": {                           # local config for bigquery db
-        #                          native_support_kwargs: {"skip_leading_rows": 1}
-        #                      },
-        #                      "redshift": {                          # local config for redshift db
-        #                            native_support_kwargs:{
-        #                               "IGNOREHEADER": 1,
-        #                               "REGION": "us-west-2",
-        #                               "IAM_ROLE": "REDSHIFT_NATIVE_LOAD_IAM_ROLE_ARN",
-        #                             }
-        #                      }
-        #                  }
-        #             }]
-        # }
-        #
-        # Final config for load_file()
-        #     1. Redshift
-        #         {
-        #             native_support_kwargs: {
-        #                 "IGNOREHEADER": 1,
-        #                 "REGION": "us-west-2",
-        #                 "IAM_ROLE": REDSHIFT_NATIVE_LOAD_IAM_ROLE_ARN".
-        #             },
-        #             "enable_native_fallback": false
-        #         }
-        #     2.  Bigquery
-        #         {
-        #             native_support_kwargs: {
-        #                 "skip_leading_rows": 1,
-        #             },
-        #             "enable_native_fallback": false
-        #         }
-        # Note - If there is a config present in both local and global config.
-        # Local config will override the global one.
-
         params.update(global_db_kwargs)
         params.update(local_db_kwargs)
-        print("params: ", params)
         my_table = aql.load_file(**params)
         aql.cleanup([my_table])
 
