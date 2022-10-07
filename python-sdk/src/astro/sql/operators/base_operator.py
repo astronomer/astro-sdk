bc51c7583ef48ee26de62328594675edac5fdd35
Daniel Imberman <daniel.imberman@gmail.com>
2022-10-07 12:11:04 -0700

    fix settings

--- python-sdk/src/astro/sql/operators/base_operator.py
+++ python-sdk/src/astro/sql/operators/base_operator.py
@@ -1,9 +1,8 @@
 from abc import ABC
 
 from airflow.models.baseoperator import BaseOperator
-from airflow.utils.log.logging_mixin import LoggingMixin
 from astro.sql.operators.upstream_task_mixin import UpstreamTaskMixin
 
 
-class AstroSQLBaseOperator(UpstreamTaskMixin, BaseOperator, LoggingMixin, ABC):
+class AstroSQLBaseOperator(UpstreamTaskMixin, BaseOperator, ABC):
     """Base class for any SQL operator that allows users to define upstream tasks using an `upstream_tasks` parameter"""
