05caa474c9058611542b2a3a64eeffac63a10ce0
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2022-10-10 13:48:30 +0100

    Use poetry to improve the local development

--- sql-cli/noxfile.py
+++ sql-cli/noxfile.py
@@ -18,17 +18,18 @@ def dev(session: nox.Session) -> None:
     session.install("nox")
     session.install("-e", "../python-sdk/.[all]")
     session.install("-e", "../python-sdk/.[tests]")
-    session.install("-e", ".[all]")
-    session.install("-e", ".[tests]")
+    session.install("poetry")
+    session.run("poetry", "install")
 
 
 @nox.session(python=["3.7", "3.8", "3.9"])
-def test(session: nox.Session) -> None:
+@nox.parametrize("airflow", ["2.3"])
+def test(session: nox.Session, airflow) -> None:
     """Run both unit and integration tests."""
     session.install("-e", "../python-sdk/.[all]")
     session.install("-e", "../python-sdk/.[tests]")
-    session.install("-e", ".[all]")
-    session.install("-e", ".[tests]")
+    session.install("poetry")
+    session.run("poetry", "install")
     # Log all the installed dependencies
     session.log("Installed Dependencies:")
     session.run("pip3", "freeze")
@@ -40,10 +41,8 @@ def test(session: nox.Session) -> None:
 @nox.session(python=["3.8"])
 def type_check(session: nox.Session) -> None:
     """Run MyPy checks."""
-    session.install("-e", "../python-sdk/.[all]")
-    session.install("-e", "../python-sdk/.[tests]")
-    session.install("-e", ".[all]")
-    session.install("-e", ".[tests]")
+    session.install("poetry")
+    session.run("poetry", "install")
     session.run("mypy", "--version")
     session.run("mypy")
 
