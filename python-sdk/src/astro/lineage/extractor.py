c7a72264859b5f643c1c4ed3d6d3fe93afe7f43d
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-10-14 19:34:57 +0545

    Add open lineage support for merge

--- python-sdk/src/astro/lineage/extractor.py
+++ python-sdk/src/astro/lineage/extractor.py
@@ -31,7 +31,7 @@ class PythonSDKExtractor(BaseExtractor):
 
     @classmethod
     def get_operator_classnames(cls) -> list[str]:
-        return ["LoadFileOperator", "AppendOperator"]
+        return ["LoadFileOperator", "AppendOperator", "MergeOperator"]
 
     def extract(self) -> TaskMetadata | None:  # skipcq: PYL-R0201
         """Empty extract implementation for the abstractmethod of the ``BaseExtractor`` class."""
