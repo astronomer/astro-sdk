0d359af29d08e2524fd03fb017f9d38774619550
Daniel Imberman <daniel.imberman@gmail.com>
2022-10-14 07:29:47 -0400

    fix warning

--- python-sdk/src/astro/custom_backend/astro_custom_backend.py
+++ python-sdk/src/astro/custom_backend/astro_custom_backend.py
@@ -35,8 +35,8 @@ class AstroCustomXcomBackend(BaseXCom):
             raise AirflowException(
                 "Since you have not provided a remote object storage conn_id, we are defaulting to "
                 "storing your data in the Metadata DB. This does not scale well and can cause degradation to "
-                "your airflow DB. Please set the AIRFLOW__ASTRO__XCOM_STORAGE_CONN_ID and "
-                "AIRFLOW__ASTRO__XCOM_STORAGE_URL variables (or you can set AIRFLOW__ASTRO__STORE_DATA_LOCAL_DEV "
+                "your airflow DB. Please set the AIRFLOW__ASTRO_SDK__XCOM_STORAGE_CONN_ID and "
+                "AIRFLOW__ASTRO_SDK__XCOM_STORAGE_URL variables (or you can set AIRFLOW__ASTRO__STORE_DATA_LOCAL_DEV "
                 "for local development"
             )
         return BaseXCom.serialize_value(value, **kwargs)
