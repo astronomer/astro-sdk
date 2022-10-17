fb6f99577bacca1a9656e9ee54d3d99c020d5017
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2022-10-17 10:18:13 +0100

    Address feedback from review

--- sql-cli/sql_cli/constants.py
+++ sql-cli/sql_cli/constants.py
@@ -2,5 +2,5 @@ CONFIG_DIR = "config"
 CONFIG_FILENAME = "configuration.yml"
 
 DEFAULT_ENVIRONMENT = "default"
-DEFAULT_AIRFLOW_HOME = f"./.airflow/{DEFAULT_ENVIRONMENT}"
-DEFAULT_DAGS_FOLDER = "./.airflow/.dags"
+DEFAULT_AIRFLOW_HOME = f".airflow/{DEFAULT_ENVIRONMENT}"
+DEFAULT_DAGS_FOLDER = ".airflow/dags"
