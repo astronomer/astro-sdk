c0f74700a5cf3176d1d21dd25d179a68ddcb494e
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-10-20 18:09:52 +0545

    Update python-sdk/src/astro/__init__.py
    Co-authored-by: Kaxil Naik <kaxilnaik@gmail.com>

--- python-sdk/src/astro/__init__.py
+++ python-sdk/src/astro/__init__.py
@@ -1,6 +1,6 @@
 """A decorator that allows users to run SQL queries natively in Airflow."""
 
-__version__ = "1.3.0dev"
+__version__ = "1.3.0.dev1"
 
 
 # This is needed to allow Airflow to pick up specific metadata fields it needs
