c7fced89e151e50f01a129d378bfffb741e6afbd
Pankaj <pankaj.singh@astronomer.io>
2022-10-26 14:25:42 +0530

    Lazy import for BaseTable

--- python-sdk/src/astro/databases/__init__.py
+++ python-sdk/src/astro/databases/__init__.py
@@ -8,6 +8,7 @@ from astro.utils.path import get_class_name, get_dict_with_module_names_to_dot_n
 
 if TYPE_CHECKING:
     from astro.databases.base import BaseDatabase
+    from astro.table import BaseTable
 
 DEFAULT_CONN_TYPE_TO_MODULE_PATH = get_dict_with_module_names_to_dot_notations(Path(__file__))
 CUSTOM_CONN_TYPE_TO_MODULE_PATH = {
@@ -21,7 +22,7 @@ CONN_TYPE_TO_MODULE_PATH = {
 SUPPORTED_DATABASES = set(DEFAULT_CONN_TYPE_TO_MODULE_PATH.keys())
 
 
-def create_database(conn_id: str, table: object | None = None, ) -> "BaseDatabase":
+def create_database(conn_id: str, table: BaseTable | None = None, ) -> "BaseDatabase":
     """
     Given a conn_id, return the associated Database class.
 
