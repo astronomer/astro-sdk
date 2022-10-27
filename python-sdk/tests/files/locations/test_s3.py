f284fe623d8e151d62a67212a89f6fe2a336c9a0
utkarsh sharma <utkarsharma2@gmail.com>
2022-10-27 18:42:34 +0530

    Fix minio connection issue

--- python-sdk/tests/files/locations/test_s3.py
+++ python-sdk/tests/files/locations/test_s3.py
@@ -41,3 +41,45 @@ def test_parse_s3_env_var():
     key, secret = S3Location._parse_s3_env_var()
     assert key == "abcd"
     assert secret == "@#$%@$#ASDH@Ksd23%SD546"
+
+
+def test_get_connection_extras():
+    """Test get_connection_extras() return correct extras"""
+    location = S3Location(path="s3://astro-sdk/imdb.csv", conn_id="minio_conn")
+    extras = location.get_connection_extras()
+    assert extras == {
+        "aws_access_key_id": "ROOTNAME",
+        "aws_secret_access_key": "CHANGEME123",
+        "endpoint_url": "http://127.0.0.1:9000",
+    }
+
+
+def test_transport_params_is_calling_get_connection_extras():
+    """
+    Test that get_connection_extras() is called when getting transport_params
+    """
+    with patch("astro.files.locations.amazon.s3.S3Location.get_connection_extras") as get_connection_extras:
+        get_connection_extras.return_value = {}
+        location = S3Location(path="s3://astro-sdk/imdb.csv", conn_id="minio_conn")
+        _ = location.transport_params
+        get_connection_extras.assert_called()
+
+
+def test_transport_params_calls_with_correct_kwargs():
+    """
+    Test that we pass correct arguments to session.client()
+    """
+
+    class Dummy:
+        def client(self, **kwargs):
+            assert kwargs == {
+                "aws_access_key_id": "ROOTNAME",
+                "aws_secret_access_key": "CHANGEME123",
+                "endpoint_url": "http://127.0.0.1:9000",
+                "service_name": "s3",
+            }
+
+    with patch("airflow.providers.amazon.aws.hooks.s3.S3Hook.get_session") as session:
+        session.return_value = Dummy()
+        location = S3Location(path="s3://astro-sdk/imdb.csv", conn_id="minio_conn")
+        _ = location.transport_params
