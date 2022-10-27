2f5b556d21a1105b4dced5f21a93e8aeae7d25dd
utkarsh sharma <utkarsharma2@gmail.com>
2022-10-27 19:20:53 +0530

    Add testcase for both cases

--- python-sdk/tests/files/locations/test_s3.py
+++ python-sdk/tests/files/locations/test_s3.py
@@ -43,9 +43,14 @@ def test_parse_s3_env_var():
     assert secret == "@#$%@$#ASDH@Ksd23%SD546"
 
 
-def test_get_connection_extras():
+@pytest.mark.parametrize(
+    "minio_conn_id",
+    ["minio_conn_1", "minio_conn_2"],
+    ids=["host", "endpoint_url"],
+)
+def test_get_connection_extras(minio_conn_id):
     """Test get_connection_extras() return correct extras"""
-    location = S3Location(path="s3://astro-sdk/imdb.csv", conn_id="minio_conn")
+    location = S3Location(path="s3://astro-sdk/imdb.csv", conn_id=minio_conn_id)
     extras = location.get_connection_extras()
     assert extras == {
         "aws_access_key_id": "ROOTNAME",
@@ -54,18 +59,28 @@ def test_get_connection_extras():
     }
 
 
-def test_transport_params_is_calling_get_connection_extras():
+@pytest.mark.parametrize(
+    "minio_conn_id",
+    ["minio_conn_1", "minio_conn_2"],
+    ids=["host", "endpoint_url"],
+)
+def test_transport_params_is_calling_get_connection_extras(minio_conn_id):
     """
     Test that get_connection_extras() is called when getting transport_params
     """
     with patch("astro.files.locations.amazon.s3.S3Location.get_connection_extras") as get_connection_extras:
         get_connection_extras.return_value = {}
-        location = S3Location(path="s3://astro-sdk/imdb.csv", conn_id="minio_conn")
+        location = S3Location(path="s3://astro-sdk/imdb.csv", conn_id=minio_conn_id)
         _ = location.transport_params
         get_connection_extras.assert_called()
 
 
-def test_transport_params_calls_with_correct_kwargs():
+@pytest.mark.parametrize(
+    "minio_conn_id",
+    ["minio_conn_1", "minio_conn_2"],
+    ids=["host", "endpoint_url"],
+)
+def test_transport_params_calls_with_correct_kwargs(minio_conn_id):
     """
     Test that we pass correct arguments to session.client()
     """
@@ -82,5 +97,5 @@ def test_transport_params_calls_with_correct_kwargs():
 
     with patch("airflow.providers.amazon.aws.hooks.s3.S3Hook.get_session") as session:
         session.return_value = Dummy()
-        location = S3Location(path="s3://astro-sdk/imdb.csv", conn_id="minio_conn")
+        location = S3Location(path="s3://astro-sdk/imdb.csv", conn_id=minio_conn_id)
         _ = location.transport_params
