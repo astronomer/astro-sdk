a7957d05dc3dd85f9ec04364b2258bd08d84f15b
feluelle <felix.uellendall@astronomer.io>
2022-11-03 09:10:41 +0100

    Make flow usage environment aware
    - set app name to flow
    - add AstroGroup and AstroCommand

--- sql-cli/sql_cli/__main__.py
+++ sql-cli/sql_cli/__main__.py
@@ -6,10 +6,17 @@ from dotenv import load_dotenv
 from rich import print as rprint
 
 import sql_cli
+from sql_cli.astro.command import AstroCommand
+from sql_cli.astro.group import AstroGroup
 from sql_cli.constants import DEFAULT_AIRFLOW_HOME, DEFAULT_DAGS_FOLDER
 
 load_dotenv()
-app = typer.Typer(add_completion=False, context_settings={"help_option_names": ["-h", "--help"]})
+app = typer.Typer(
+    name="flow",
+    cls=AstroGroup,
+    add_completion=False,
+    context_settings={"help_option_names": ["-h", "--help"]},
+)
 
 
 airflow_logger = logging.getLogger("airflow")
@@ -17,17 +24,26 @@ airflow_logger.setLevel(logging.CRITICAL)
 airflow_logger.propagate = False
 
 
-@app.command(help="Print the SQL CLI version.")
+@app.command(
+    cls=AstroCommand,
+    help="Print the SQL CLI version.",
+)
 def version() -> None:
     rprint("Astro SQL CLI", sql_cli.__version__)
 
 
-@app.command(help="Print additional information about the project.")
+@app.command(
+    cls=AstroCommand,
+    help="Print additional information about the project.",
+)
 def about() -> None:
     rprint("Find out more: https://github.com/astronomer/astro-sdk/sql-cli")
 
 
-@app.command(help="Generate the Airflow DAG from a directory of SQL files.")
+@app.command(
+    cls=AstroCommand,
+    help="Generate the Airflow DAG from a directory of SQL files.",
+)
 def generate(
     workflow_name: str = typer.Argument(
         default=...,
@@ -53,9 +69,10 @@ def generate(
 
 
 @app.command(
+    cls=AstroCommand,
     help="""
     Validate Airflow connection(s) provided in the configuration file for the given environment.
-    """
+    """,
 )
 def validate(
     project_dir: Path = typer.Argument(
@@ -89,10 +106,11 @@ def validate(
 
 
 @app.command(
+    cls=AstroCommand,
     help="""
     Run a workflow locally. This task assumes that there is a local airflow DB (can be a SQLite file), that has been
     initialized with Airflow tables.
-    """
+    """,
 )
 def run(
     workflow_name: str = typer.Argument(
@@ -135,6 +153,7 @@ def run(
 
 
 @app.command(
+    cls=AstroCommand,
     help="""
     Initialise a project structure to write workflows using SQL files.
 
@@ -163,7 +182,7 @@ def run(
     * Update the file `config/default/configuration.yaml` to declare database connections.
     \b\n
     * Create SQL workflows within the `workflows` folder.
-    """
+    """,
 )
 def init(
     project_dir: Path = typer.Argument(
---
+++ sql-cli/sql_cli/astro/group.py
@@ -0,0 +1,18 @@
+import os
+
+import click
+import typer
+from typer.core import TyperGroup
+
+
+class AstroGroup(TyperGroup):
+    def format_usage(self, ctx: typer.Context, formatter: click.HelpFormatter) -> None:
+        """Writes the usage line into the formatter.
+
+        This is a low-level method called by :meth:`get_usage`.
+        """
+        pieces = self.collect_usage_pieces(ctx)
+        command_path = ctx.command_path
+        if os.getenv("ASTRO_CLI"):
+            command_path = " ".join(["astro", ctx.command_path])
+        formatter.write_usage(command_path, " ".join(pieces))
