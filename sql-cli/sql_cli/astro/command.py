fb5ef6726adce35926cee6c999aaa530c2109954
feluelle <felix.uellendall@astronomer.io>
2022-11-04 12:39:19 +0100

    Refactor code and add comment

--- sql-cli/sql_cli/astro/command.py
+++ sql-cli/sql_cli/astro/command.py
@@ -1,8 +1,8 @@
-import os
-
 import click
 import typer
 from typer.core import TyperCommand
+
+from sql_cli.astro.utils import resolve_command_path
 
 
 class AstroCommand(TyperCommand):
@@ -12,7 +12,4 @@ class AstroCommand(TyperCommand):
         This is a low-level method called by :meth:`get_usage`.
         """
         pieces = self.collect_usage_pieces(ctx)
-        command_path = ctx.command_path
-        if os.getenv("ASTRO_CLI"):
-            command_path = " ".join(["astro", ctx.command_path])
-        formatter.write_usage(command_path, " ".join(pieces))
+        formatter.write_usage(resolve_command_path(ctx.command_path), " ".join(pieces))
--- sql-cli/sql_cli/astro/group.py
+++ sql-cli/sql_cli/astro/group.py
@@ -1,8 +1,8 @@
-import os
-
 import click
 import typer
 from typer.core import TyperGroup
+
+from sql_cli.astro.utils import resolve_command_path
 
 
 class AstroGroup(TyperGroup):
@@ -12,7 +12,4 @@ class AstroGroup(TyperGroup):
         This is a low-level method called by :meth:`get_usage`.
         """
         pieces = self.collect_usage_pieces(ctx)
-        command_path = ctx.command_path
-        if os.getenv("ASTRO_CLI"):
-            command_path = " ".join(["astro", ctx.command_path])
-        formatter.write_usage(command_path, " ".join(pieces))
+        formatter.write_usage(resolve_command_path(ctx.command_path), " ".join(pieces))
