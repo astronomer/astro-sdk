511714c0687b7be585a7c5b539c46a78086464cc
Pankaj <pankaj.singh@astronomer.io>
2022-11-08 17:38:43 +0530

    Fix typo

--- python-sdk/src/astro/databases/aws/redshift.py
+++ python-sdk/src/astro/databases/aws/redshift.py
@@ -246,7 +246,7 @@ class RedshiftDatabase(BaseDatabase):
             handler=lambda x: [y[0] for y in x.fetchall()],
         )
         target_table_all_columns = self.hook.run(  # skipcq BAN-B608
-            f"select col_name from pg_get_cols('{source_table_name}') "
+            f"select col_name from pg_get_cols('{target_table_name}') "
             f"cols(view_schema name, view_name name, col_name name, col_type varchar, col_num int);",
             handler=lambda x: [y[0] for y in x.fetchall()],
         )
