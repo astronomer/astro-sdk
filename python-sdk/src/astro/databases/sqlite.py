3ac8126e165f0e82988fbbf6725f3faa1efdceee
Pankaj <pankaj.singh@astronomer.io>
2022-11-12 01:11:04 +0530

    Raise error if dataframe is empty

--- python-sdk/src/astro/databases/sqlite.py
+++ python-sdk/src/astro/databases/sqlite.py
@@ -2,12 +2,13 @@ from __future__ import annotations
 
 import socket
 
+import pandas as pd
 from airflow.providers.sqlite.hooks.sqlite import SqliteHook
 from sqlalchemy import MetaData as SqlaMetaData, create_engine
 from sqlalchemy.engine.base import Engine
 from sqlalchemy.sql.schema import Table as SqlaTable
 
-from astro.constants import MergeConflictStrategy
+from astro.constants import DEFAULT_CHUNK_SIZE, LoadExistStrategy, MergeConflictStrategy
 from astro.databases.base import BaseDatabase
 from astro.table import BaseTable, Metadata
 
@@ -83,6 +84,26 @@ class SqliteDatabase(BaseDatabase):
         Handles database-specific logic to handle index for Sqlite.
         """
         return "CREATE UNIQUE INDEX merge_index ON {{table}}(%s)" % ",".join(parameters)  # skipcq PYL-C0209
+
+    def load_pandas_dataframe_to_table(
+        self,
+        source_dataframe: pd.DataFrame,
+        target_table: BaseTable,
+        if_exists: LoadExistStrategy = "replace",
+        chunk_size: int = DEFAULT_CHUNK_SIZE,
+    ) -> None:
+        """
+        Create a table with the dataframe's contents.
+        If the table already exists, append or replace the content, depending on the value of `if_exists`.
+
+        :param source_dataframe: Local or remote filepath
+        :param target_table: Table in which the file will be loaded
+        :param if_exists: Strategy to be used in case the target table already exists.
+        :param chunk_size: Specify the number of rows in each batch to be written at a time.
+        """
+        if source_dataframe.empty:
+            raise ValueError("Can't load empty dataframe")
+        super().load_pandas_dataframe_to_table(source_dataframe, target_table, if_exists, chunk_size)
 
     def merge_table(
         self,
