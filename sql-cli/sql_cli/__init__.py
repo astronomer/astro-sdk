606975680f89bae3b73be270c26b97217aee10fd
feluelle <felix.uellendall@astronomer.io>
2022-11-17 14:28:08 +0100

    Fix error help for astro-cli
    Monkey-patch rich_format_error to make it environment aware. So that when running in astro-cli it says:
    > Try 'astro flow -h' for help.

--- sql-cli/sql_cli/__init__.py
+++ sql-cli/sql_cli/__init__.py
@@ -1,6 +1,13 @@
 import importlib.metadata
 import os
 
+import typer.rich_utils
+
+from sql_cli.utils.rich import rich_format_error
+
+# We monkey-patch rich_format_error to make it environment aware
+typer.rich_utils.rich_format_error = rich_format_error
+
 # TODO: Remove after the `astro-sdk-python` package 1.3 is released
 os.environ["AIRFLOW__CORE__ENABLE_XCOM_PICKLING"] = "True"
 __version__ = importlib.metadata.version("astro-sql-cli")
