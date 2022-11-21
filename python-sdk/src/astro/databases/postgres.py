c074681e5923c17bd7b44830ed31be495bec2438
Pankaj <pankaj.singh@astronomer.io>
2022-11-21 13:54:49 +0530

    Fix tests

--- python-sdk/src/astro/databases/postgres.py
+++ python-sdk/src/astro/databases/postgres.py
@@ -42,7 +42,7 @@ class PostgresDatabase(BaseDatabase):
         conn = PostgresHook(postgres_conn_id=self.conn_id).get_connection(self.conn_id)
         kwargs = {}
         if (conn.schema is None) and (self.table and self.table.metadata and self.table.metadata.database):
-            kwargs.update({"database": self.table.metadata.schema})
+            kwargs.update({"database": self.table.metadata.database})
         return PostgresHook(postgres_conn_id=self.conn_id, **kwargs)
 
     @property
