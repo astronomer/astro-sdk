01fc85fe50a704b5bef2d24174433338be193468
Pankaj Singh <98807258+pankajastro@users.noreply.github.com>
2022-11-21 16:43:49 +0530

    Fix database setting in PostgresDatabase (#1276)
    # Description
    
    ## What is the current behavior?
    
    Currently, the CI is failing for postgres related tests, because the
    postgres database is set to the table schema.
    
    **More details:**
    
    In
    https://github.com/astronomer/astro-sdk/commit/313f6da5fc572a8a35c6fa3db90373a7586be4de#diff-9fcbc9dc6fc68bf0a0d62a485508779f5b1f6d749a197e0f1ab1758983d1eb1e
    we added `kwargs.update({"database"` but PostgresHook did not support
    `database` kwarg at that time. This is why it basically did not do
    anything. Now after https://github.com/apache/airflow/pull/26744 got
    released in the latest provider, it is being picked up and it shows that
    we wrongly set `{"database": self.table.metadata.schema}`
    
    ## What is the new behavior?
    
    We correctly set the database to the table's database.
    
    ## Does this introduce a breaking change?
    
    No.
    
    ### Checklist
    - [ ] Created tests which fail without the change (if possible)
    - [ ] Extended the README / documentation, if necessary
    
    (cherry picked from commit b452f15fdb034863d6b0a38bc2423a13b6caf51e)

--- python-sdk/src/astro/databases/postgres.py
+++ python-sdk/src/astro/databases/postgres.py
@@ -41,8 +41,8 @@ class PostgresDatabase(BaseDatabase):
         """Retrieve Airflow hook to interface with the Postgres database."""
         conn = PostgresHook(postgres_conn_id=self.conn_id).get_connection(self.conn_id)
         kwargs = {}
-        if (conn.schema is None) and (self.table and self.table.metadata and self.table.metadata.schema):
-            kwargs.update({"database": self.table.metadata.schema})
+        if (conn.schema is None) and (self.table and self.table.metadata and self.table.metadata.database):
+            kwargs.update({"database": self.table.metadata.database})
         return PostgresHook(postgres_conn_id=self.conn_id, **kwargs)
 
     @property
