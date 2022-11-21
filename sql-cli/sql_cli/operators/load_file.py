88424ec82534e104effe5fd5f25228de8c688347
feluelle <felix.uellendall@astronomer.io>
2022-11-21 17:28:47 +0100

    Add docstrings

--- sql-cli/sql_cli/operators/load_file.py
+++ sql-cli/sql_cli/operators/load_file.py
@@ -7,24 +7,42 @@ from astro.sql import LoadFileOperator
 
 
 def build_load_file_operator_content(
-    operator_content: dict[str, Any], operator_file_name: str
+    operator_content: dict[str, Any],
+    operator_file_name: str,
 ) -> dict[str, Any]:
+    """
+    Deserialize the File and Table classes.
+
+    :param operator_content: The dictionary of the operator content to be deserialized.
+    :param operator_file_name: The file name of the operator.
+
+    :returns: the operator content deserialized.
+    """
     input_file_content = operator_content.pop("input_file")
     input_file_content["class"] = "File"
     input_file_content.setdefault("is_dataframe", False)
     input_file_content.setdefault("normalize_config", None)
-    input_file = deserialize(input_file_content)
-    operator_content["input_file"] = input_file
+
+    operator_content["input_file"] = deserialize(input_file_content)
+
     output_table_content = operator_content.pop("output_table")
     output_table_content["class"] = "Table"
     output_table_content.setdefault("name", operator_file_name)
     output_table_content.setdefault("temp", False)
-    output_table = deserialize(output_table_content)
-    operator_content["output_table"] = output_table
+
+    operator_content["output_table"] = deserialize(output_table_content)
+
     return operator_content
 
 
 def get_load_file_instance(yaml_content: dict[str, Any], operator_file_name: str) -> LoadFileOperator:
+    """
+    Instantiate the LoadFileOperator.
+
+    :param yaml_content: The content of the load_file yaml.
+    :param operator_file_name: The file name of the operator.
+
+    :returns: an instance of the LoadFileOperator
+    """
     operator_content: dict[str, Any] = build_load_file_operator_content(yaml_content, operator_file_name)
-    load_file_operator = LoadFileOperator(**operator_content)
-    return load_file_operator
+    return LoadFileOperator(**operator_content)
