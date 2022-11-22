6dc24f1673d2147eaa1a5c037942ec799e768896
Pankaj <pankaj.singh@astronomer.io>
2022-11-23 00:05:58 +0530

    Fix tests

--- python-sdk/tests/databases/test_postgres.py
+++ python-sdk/tests/databases/test_postgres.py
@@ -155,7 +155,7 @@ def test_load_pandas_dataframe_to_table_with_append(database_table_fixture):
         if_exists="append",
     )
 
-    statement = f"SELECT * FROM {table.name};"
+    statement = f"SELECT * FROM {database.get_table_qualified_name(table)};"
     response = database.run_sql(statement)
 
     rows = response.fetchall()
@@ -169,7 +169,7 @@ def test_load_pandas_dataframe_to_table_with_append(database_table_fixture):
         if_exists="append",
     )
 
-    statement = f"SELECT * FROM {table.name};"
+    statement = f"SELECT * FROM {database.get_table_qualified_name(table)};"
     response = database.run_sql(statement)
     rows = response.fetchall()
     assert len(rows) == 4
@@ -198,7 +198,7 @@ def test_load_pandas_dataframe_to_table_with_replace(database_table_fixture):
         target_table=table,
     )
 
-    statement = f"SELECT * FROM {table.name};"
+    statement = f"SELECT * FROM {database.get_table_qualified_name(table)};"
     response = database.run_sql(statement)
 
     rows = response.fetchall()
@@ -212,7 +212,7 @@ def test_load_pandas_dataframe_to_table_with_replace(database_table_fixture):
         target_table=table,
     )
 
-    statement = f"SELECT * FROM {table.name};"
+    statement = f"SELECT * FROM {database.get_table_qualified_name(table)};"
     response = database.run_sql(statement)
     rows = response.fetchall()
     assert len(rows) == 2
