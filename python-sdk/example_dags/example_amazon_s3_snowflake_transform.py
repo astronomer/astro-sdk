84a4f2399a096f6587464fbd53f2db2bd1e4f72c
rajaths010494 <rajath.srinivasaiah@astronomer.io>
2022-11-22 18:24:44 +0530

    delete tables snowflake

--- python-sdk/example_dags/example_amazon_s3_snowflake_transform.py
+++ python-sdk/example_dags/example_amazon_s3_snowflake_transform.py
@@ -1,39 +1,51 @@
 import os
-import time
 from datetime import datetime, timedelta
-
-import pandas as pd
 
 # Uses data from https://www.kaggle.com/c/shelter-animal-outcomes
 from airflow.decorators import dag
 
 from astro import sql as aql
-from astro.files import File
-from astro.table import Metadata, Table
+from astro.table import Table
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+FILE_PATH = dir_path + "/data/"
+
+# @aql.transform()
+# def combine_data(center_1: Table, center_2: Table):
+#     return """SELECT * FROM {{center_1}}
+#     UNION SELECT * FROM {{center_2}}"""
 
 
-@aql.transform()
-def combine_data(center_1: Table, center_2: Table):
-    return """SELECT * FROM {{center_1}}
-    UNION SELECT * FROM {{center_2}}"""
+# @aql.transform()
+# def clean_data(input_table: Table):
+#     return """SELECT *
+#     FROM {{input_table}} WHERE type NOT LIKE 'Guinea Pig'
+#     """
 
 
-@aql.transform()
-def clean_data(input_table: Table):
-    return """SELECT *
-    FROM {{input_table}} WHERE type NOT LIKE 'Guinea Pig'
-    """
+# # [START dataframe_example_1]
+# @aql.dataframe(columns_names_capitalization="original")
+# def aggregate_data(df: pd.DataFrame):
+#     new_df = df.pivot_table(index="date", values="name", columns=["type"], aggfunc="count").reset_index()
+#     new_df.columns = new_df.columns.str.lower()
+#     return new_df
 
 
-# [START dataframe_example_1]
-@aql.dataframe(columns_names_capitalization="original")
-def aggregate_data(df: pd.DataFrame):
-    new_df = df.pivot_table(index="date", values="name", columns=["type"], aggfunc="count").reset_index()
-    new_df.columns = new_df.columns.str.lower()
-    return new_df
+# # [END dataframe_example_1]
 
 
-# [END dataframe_example_1]
+@aql.run_raw_sql()
+def raw_sql_query(table_name: Table):
+    return """DROP TABLE IF EXISTS {{table_name}};"""
+
+
+def get_temp_tables():
+    with open(FILE_PATH + "delete_test.txt") as file_in:
+        lines = []
+        for line in file_in:
+            lines.append(line)
+    return lines
 
 
 @dag(
@@ -49,55 +61,61 @@ def aggregate_data(df: pd.DataFrame):
 )
 def example_amazon_s3_snowflake_transform():
 
-    s3_bucket = os.getenv("S3_BUCKET", "s3://tmp9")
+    # s3_bucket = os.getenv("S3_BUCKET", "s3://tmp9")
 
-    input_table_1 = Table(
-        name="ADOPTION_CENTER_1",
-        metadata=Metadata(
-            database=os.environ["SNOWFLAKE_DATABASE"],
-            schema=os.environ["SNOWFLAKE_SCHEMA"],
-        ),
-        conn_id="snowflake_conn",
-    )
-    # [START metadata_example_snowflake]
-    input_table_2 = Table(
-        name="ADOPTION_CENTER_2",
-        metadata=Metadata(
-            database=os.environ["SNOWFLAKE_DATABASE"],
-            schema=os.environ["SNOWFLAKE_SCHEMA"],
-        ),
-        conn_id="snowflake_conn",
-    )
-    # [END metadata_example_snowflake]
+    # input_table_1 = Table(
+    #     name="ADOPTION_CENTER_1",
+    #     metadata=Metadata(
+    #         database=os.environ["SNOWFLAKE_DATABASE"],
+    #         schema=os.environ["SNOWFLAKE_SCHEMA"],
+    #     ),
+    #     conn_id="snowflake_conn",
+    # )
+    # # [START metadata_example_snowflake]
+    # input_table_2 = Table(
+    #     name="ADOPTION_CENTER_2",
+    #     metadata=Metadata(
+    #         database=os.environ["SNOWFLAKE_DATABASE"],
+    #         schema=os.environ["SNOWFLAKE_SCHEMA"],
+    #     ),
+    #     conn_id="snowflake_conn",
+    # )
+    # # [END metadata_example_snowflake]
 
-    temp_table_1 = aql.load_file(
-        input_file=File(path=f"{s3_bucket}/ADOPTION_CENTER_1_unquoted.csv"),
-        output_table=input_table_1,
-    )
-    temp_table_2 = aql.load_file(
-        input_file=File(path=f"{s3_bucket}/ADOPTION_CENTER_2_unquoted.csv"),
-        output_table=input_table_2,
-    )
+    # temp_table_1 = aql.load_file(
+    #     input_file=File(path=f"{s3_bucket}/ADOPTION_CENTER_1_unquoted.csv"),
+    #     output_table=input_table_1,
+    # )
+    # temp_table_2 = aql.load_file(
+    #     input_file=File(path=f"{s3_bucket}/ADOPTION_CENTER_2_unquoted.csv"),
+    #     output_table=input_table_2,
+    # )
 
-    combined_data = combine_data(
-        center_1=temp_table_1,
-        center_2=temp_table_2,
-    )
+    # combined_data = combine_data(
+    #     center_1=temp_table_1,
+    #     center_2=temp_table_2,
+    # )
 
-    cleaned_data = clean_data(combined_data)
-    # [START dataframe_example_2]
-    aggregate_data(
-        cleaned_data,
-        output_table=Table(
-            name="aggregated_adoptions_" + str(int(time.time())),
-            metadata=Metadata(
-                schema=os.environ["SNOWFLAKE_SCHEMA"],
-                database=os.environ["SNOWFLAKE_DATABASE"],
-            ),
-            conn_id="snowflake_conn",
-        ),
-    )
-    # [END dataframe_example_2]
+    # cleaned_data = clean_data(combined_data)
+    # # [START dataframe_example_2]
+    # aggregate_data(
+    #     cleaned_data,
+    #     output_table=Table(
+    #         name="aggregated_adoptions_" + str(int(time.time())),
+    #         metadata=Metadata(
+    #             schema=os.environ["SNOWFLAKE_SCHEMA"],
+    #             database=os.environ["SNOWFLAKE_DATABASE"],
+    #         ),
+    #         conn_id="snowflake_conn",
+    #     ),
+    # )
+    # # [END dataframe_example_2]
+
+    lines = get_temp_tables()
+    for i in lines:
+        homes_reporting = Table(name=i, conn_id="snowflake_conn")
+        raw_sql_query(homes_reporting)
+
     aql.cleanup()
 
 
