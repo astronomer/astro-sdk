8a544fa88813d824775b28f901457128b848762e
Pankaj Koti <pankaj.koti@astronomer.io>
2022-11-24 09:24:49 +0530

    Use sqlite conn for load file workflow example (#1303)
    ## What is the current behavior?
    <!-- Please describe the current behavior that you are modifying. -->
    Previously, we were using bigquery conn to load data from GCS to
    Bigquery
    
    <!--
    Issues are required for both bug fixes and features.
    Reference it using one of the following:
    
    closes: #ISSUE
    related: #ISSUE
    -->
    closes: #1282 
    
    ## What is the new behavior?
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    With #1282 we aim to keep it simple by not having to create complex
    tests for using our example load workflow. The PR replaces the existing
    load file workflow to load data from a github file to load data into
    sqlite database.
    
    We additionally are fixing minor issues where the File and Table
    deserializers are pased void data values for their optional keys so that
    the deserializer does not raise KeyError.
    
    ## Does this introduce a breaking change?
    No
    
    ### Checklist
    - [x] Created tests which fail without the change (if possible)
    - [ ] Extended the README / documentation, if necessary

--- sql-cli/conftest.py
+++ sql-cli/conftest.py
@@ -1,4 +1,3 @@
-import os
 import random
 import shutil
 import string
@@ -12,7 +11,6 @@ from airflow.utils.session import create_session
 from astro.table import MAX_TABLE_NAME_LENGTH
 from sql_cli.dag_generator import Workflow
 from sql_cli.project import Project
-from sql_cli.utils.jinja import render
 from sql_cli.workflow_directory_parser import SqlFile, WorkflowFile
 
 CWD = Path(__file__).parent
@@ -51,11 +49,6 @@ def root_directory_unsupported_operator():
 @pytest.fixture()
 def root_directory_dags():
     return CWD / "tests" / "test_dag"
-
-
-@pytest.fixture()
-def root_directory_templates_load_file():
-    return CWD / "tests" / "templates" / "load_file"
 
 
 @pytest.fixture()
@@ -179,44 +172,6 @@ def initialised_project_with_test_config(initialised_project: Project):
         src=CWD / "tests" / "config" / "test",
         dst=initialised_project.directory / "config" / "test",
     )
-    return initialised_project
-
-
-@pytest.fixture()
-def initialised_project_with_load_file_workflow(
-    root_directory_templates_load_file, initialised_project: Project
-):
-    # We use bigquery connection for the load_file operator example workflow. Although, we create isolated project
-    # directories, the workflow yaml file refer to the same output table name. And hence, upon running concurrent tests
-    # across Airflow versions and python versions, they try to access the same output table and conflict each other.
-    # We need to create unique output table name in the YAML and also modify the created workflow file name to use the
-    # same name as the output table name as the subsequent workflow SQL file needs to refer this YAML file.
-    # We would not need this fixture and the below manipulations once issue #1282 is addressed.
-    output_table_name = create_unique_table_name(UNIQUE_HASH_SIZE)
-    render_kwargs = {
-        "context": {"output_table_name": output_table_name},
-        "searchpath": root_directory_templates_load_file,
-    }
-
-    render(
-        template_file=Path("load_homes_main.yaml.jinja2"),
-        output_file=initialised_project.directory
-        / "workflows"
-        / "example_load_file"
-        / f"{output_table_name}.yaml",
-        **render_kwargs,
-    )
-    os.remove(initialised_project.directory / "workflows" / "example_load_file" / "load_homes_main.yaml")
-
-    render(
-        template_file=Path("transform_homes_main.sql.jinja2"),
-        output_file=initialised_project.directory
-        / "workflows"
-        / "example_load_file"
-        / "transform_homes_main.sql",
-        **render_kwargs,
-    )
-
     return initialised_project
 
 
