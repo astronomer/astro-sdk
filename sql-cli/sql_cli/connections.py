8abd1d7d7fe1f6e2f9a2bf07df40165c2c0fa4f8
Kaxil Naik <kaxilnaik@gmail.com>
2022-12-05 17:05:12 +0000

    Use env vars to create connections (#1125)
    Instead of using session and storing connections in the database, we can
    simply store the connections as Environment Variables.
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: feluelle <felix.uellendall@astronomer.io>
    Co-authored-by: Felix Uellendall <feluelle@users.noreply.github.com>

--- sql-cli/sql_cli/connections.py
+++ sql-cli/sql_cli/connections.py
@@ -1,25 +1,13 @@
 from __future__ import annotations
 
+import os
 from pathlib import Path
 
 from airflow.models import Connection
-from airflow.utils.session import create_session
 
 from sql_cli.utils.rich import rprint
 
 CONNECTION_ID_OUTPUT_STRING_WIDTH = 25
-
-
-def _create_or_replace_connection(conn_obj: Connection) -> None:
-    """Creates a new or replaces existing connection in the Airflow DB with the given connection object."""
-    conn_id = conn_obj.conn_id
-    with create_session() as session:
-        db_connection = session.query(Connection).filter_by(conn_id=conn_id).one_or_none()
-        if db_connection:
-            session.delete(db_connection)
-            session.commit()
-        session.add(conn_obj)
-        session.commit()
 
 
 def validate_connections(connections: list[Connection], connection_id: str | None = None) -> None:
@@ -30,9 +18,9 @@ def validate_connections(connections: list[Connection], connection_id: str | Non
     config_file_contains_connection = False
 
     for connection in connections:
-        if connection.id == connection_id:
+        if connection.conn_id == connection_id:
             config_file_contains_connection = True
-        _create_or_replace_connection(connection)
+        os.environ[f"AIRFLOW_CONN_{connection.conn_id.upper()}"] = connection.get_uri()
         status = "[bold green]PASSED[/bold green]" if _is_valid(connection) else "[bold red]FAILED[/bold red]"
         rprint(f"Validating connection {connection.conn_id:{CONNECTION_ID_OUTPUT_STRING_WIDTH}}", status)
 
--- sql-cli/tests/utils.py
+++ sql-cli/tests/utils.py
@@ -1,14 +1,31 @@
+from __future__ import annotations
+
 from pathlib import Path
-from typing import List
+
+from airflow.models import Connection
 
 
-def list_dir(dir_name: str) -> List[Path]:
+def list_dir(dir_name: str) -> list[Path]:
     """
     Return sorted list of files and directories available in the given directory.
 
     :param dir_name: Source directory name
 
     :returns: Sorted list of files and directories within the given directory.
-
     """
     return sorted([path.relative_to(dir_name) for path in Path(dir_name).rglob("*")])
+
+
+def get_connection_by_id(connections: list[Connection], connection_id: str) -> Connection | None:
+    """
+    Get a connection by id.
+
+    :param connections: The connections to iterate through.
+    :param connection_id: The id of the connection to look for.
+
+    :returns: the connection object of the id if exists.
+    """
+    for connection in connections:
+        if connection.conn_id == connection_id:
+            return connection
+    return None
