c4e0813dde1b51b96fd8a72d708e7d524473f10f
rajaths010494 <rajath.srinivasaiah@astronomer.io>
2022-12-08 18:24:04 +0530

    add row_count check

--- python-sdk/tests/sql/operators/test_merge.py
+++ python-sdk/tests/sql/operators/test_merge.py
@@ -198,7 +198,7 @@ def test_merge_with_the_same_schema(database_table_fixture, multiple_tables_fixt
             {"id": 4, "name": "Czwarte imię"},
         ]
     )
-
+    assert first_table.row_count == 4
     test_utils.assert_dataframes_are_equal(computed, expected)
 
 
@@ -261,7 +261,7 @@ def test_merge_with_different_schemas(database_table_fixture, multiple_tables_fi
             {"id": 4, "name": "Czwarte imię"},
         ]
     )
-
+    assert first_table.row_count == 4
     test_utils.assert_dataframes_are_equal(computed, expected)
 
 
