4b8c2a2f9f551bca54a7066721da1411f4de836e
Luan Moreno <luanmorenomaciel@hotmail.com>
2022-12-16 18:54:32 -0300

    Fix Connection and Integration Tests

--- python-sdk/tests_integration/files/locations/test_wasb.py
+++ python-sdk/tests_integration/files/locations/test_wasb.py
@@ -8,26 +8,6 @@ from astro.files.locations import create_file_location
 from astro.files.locations.azure.wasb import WASBLocation
 
 
-def test_get_transport_params_with_wasb():
-    """test get_transport_params() method with WASB filepath"""
-    path = "wasbs://container@storageaccount.blob.core.windows.net"
-    location = create_file_location(path)
-    credentials = location.transport_params
-    assert isinstance(credentials["client"], BlobServiceClient)
-
-
-@patch(
-    "airflow.providers.microsoft.azure.hooks.wasb.WasbHook.get_blobs_list",
-    return_value=["house1.csv", "house2.csv"],
-)
-def test_remote_object_store_prefix(remote_file):
-    """with remote filepath having prefix"""
-    location = create_file_location("wasb://tmp/house")
-    assert isinstance(location, WASBLocation)
-    assert sorted(location.paths) == sorted(["wasb://tmp/house1.csv", "wasb://tmp/house2.csv"])
-
-
-@pytest.mark.integration
 @pytest.mark.parametrize(
     "remote_files_fixture",
     [{"provider": "azure", "file_count": 2, "conn_id": "wasb_default_conn"}],
