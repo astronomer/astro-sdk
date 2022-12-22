f36c537402cb4bb26df2aa279698882208105b5e
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-12-22 17:16:34 +0545

    Update new dataset based on File, Table, Dataframe and API dataset

--- python-sdk/example_dags/example_universal_transfer_operator.py
+++ python-sdk/example_dags/example_universal_transfer_operator.py
@@ -2,7 +2,8 @@ from datetime import datetime
 
 from airflow import DAG
 from transfers.constants import TransferMode
-from transfers.datasets.base import UniversalDataset as Dataset
+from transfers.datasets.file import File
+from transfers.datasets.table import Table
 from transfers.universal_transfer_operator import UniversalTransferOperator
 
 START_DATE = datetime(2000, 1, 1)
@@ -14,14 +15,14 @@ with DAG(
 ) as dag:
     uto_task = UniversalTransferOperator(
         task_id="uto",
-        source_dataset=Dataset("gs://uto-test/uto/", conn_id="google_cloud_default", extra={}),
-        destination_dataset=Dataset("s3://astro-sdk-test/uto/", conn_id="aws_default", extra={}),
+        source_dataset=File("gs://uto-test/uto/", conn_id="google_cloud_default", extra={}),
+        destination_dataset=File("s3://astro-sdk-test/uto/", conn_id="aws_default", extra={}),
     )
 
     uto_fivetran_with_connector_id = UniversalTransferOperator(
         task_id="uto_fivetran_with_connector_id",
-        source_dataset=Dataset("s3://astro-sdk-test/uto/", conn_id="aws_default", extra={}),
-        destination_dataset=Dataset(
+        source_dataset=File("s3://astro-sdk-test/uto/", conn_id="aws_default", extra={}),
+        destination_dataset=Table(
             "snowflake://gp21411.us-east-1.snowflakecomputing.com/providers_fivetran_dev.s3.fivetran_ankit_test",
             conn_id="snowflake_default",
             extra={},
@@ -35,8 +36,8 @@ with DAG(
 
     uto_fivetran_without_connector_id = UniversalTransferOperator(
         task_id="uto_fivetran_without_connector_id",
-        source_dataset=Dataset("s3://astro-sdk-test/uto/", conn_id="aws_default", extra={}),
-        destination_dataset=Dataset(
+        source_dataset=File("s3://astro-sdk-test/uto/", conn_id="aws_default", extra={}),
+        destination_dataset=Table(
             "snowflake://{account name}/{database}.{schema}.{table}", conn_id="snowflake_default", extra={}
         ),
         transfer_mode=TransferMode.THIRDPARTY,
