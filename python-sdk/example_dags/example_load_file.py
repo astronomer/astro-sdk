2745943cfc926ff313eb1dcd9f419d19a92921cc
rajaths010494 <rajath.srinivasaiah@astronomer.io>
2022-12-22 12:12:30 +0530

    SFTP support for location

--- python-sdk/example_dags/example_load_file.py
+++ python-sdk/example_dags/example_load_file.py
@@ -253,4 +253,17 @@ with dag:
     )
     # [END load_file_example_19]
 
+    # [START load_file_example_20]
+    aql.load_file(
+        input_file=File(path="sftp://upload/", conn_id="sftp_conn", filetype=FileType.CSV),
+        output_table=Table(
+            conn_id=SNOWFLAKE_CONN_ID,
+            metadata=Metadata(
+                database=os.environ["SNOWFLAKE_DATABASE"],
+                schema=os.environ["SNOWFLAKE_SCHEMA"],
+            ),
+        ),
+    )
+    # [END load_file_example_20]
+
     aql.cleanup()
---
+++ python-sdk/src/astro/files/locations/sftp/sftp.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+import os
+from urllib.parse import urlparse
+
+from airflow.providers.sftp.hooks.sftp import SFTPHook
+
+from astro.constants import FileLocation
+from astro.exceptions import PermissionNotSetError
+from astro.files.locations.base import BaseFileLocation
+
+
+class SFTPLocation(BaseFileLocation):
+    """Handler SFTP object store operations"""
+
+    location_type = FileLocation.SFTP
+
+    @property
+    def hook(self) -> SFTPHook:
+        return SFTPHook(ssh_conn_id=self.conn_id) if self.conn_id else SFTPHook()
+
+    @property
+    def transport_params(self) -> dict:
+        """get SFTP credentials for remote file system"""
+        client = self.hook.get_connection(self.conn_id)
+        extra_options = client.extra_dejson
+        if "key_file" in extra_options:
+            key_file = extra_options.get("key_file")
+            return {"connect_kwargs": {"key_filename": key_file}}
+        elif client.password:
+            return {"connect_kwargs": {"password": client.password}}
+        raise PermissionNotSetError("SFTP credentials were not set in the connection.")
+
+    @property
+    def paths(self) -> list[str]:
+        """Resolve SFTP file paths with prefix"""
+        url = urlparse(self.path)
+        client = self.hook.get_connection(self.conn_id)
+        uri = client.get_uri()
+        if self.hook.isdir(url.netloc + url.path):
+            prefixes = self.hook.list_directory(url.netloc + url.path)
+            paths = [uri + "/" + url.netloc + os.path.join(url.path, keys) for keys in prefixes]
+        else:
+            paths = [uri + "/" + url.netloc + url.path]
+        return paths
+
+    @property
+    def size(self) -> int:
+        """Return file size for SFTP location"""
+        url = urlparse(self.path)
+        conn = self.hook.get_conn()
+        stat = conn.stat(url.path).st_size
+        return int(stat) or -1
+
+    @property
+    def openlineage_dataset_namespace(self) -> str:
+        """
+        Returns the open lineage dataset namespace as per
+        https://github.com/OpenLineage/OpenLineage/blob/main/spec/Naming.md
+        """
+        parsed_url = urlparse(self.path)
+        return f"{parsed_url.scheme}://{parsed_url.netloc}"
+
+    @property
+    def openlineage_dataset_name(self) -> str:
+        """
+        Returns the open lineage dataset name as per
+        https://github.com/OpenLineage/OpenLineage/blob/main/spec/Naming.md
+        """
+        return urlparse(self.path).path
