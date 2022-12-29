dacfcfd784b058921d8655790f9cd9a94ef4e6e1
rajaths010494 <rajath.srinivasaiah@astronomer.io>
2022-12-29 17:40:46 +0530

    Add test for all filetypes

--- python-sdk/tests_integration/files/locations/test_sftp.py
+++ python-sdk/tests_integration/files/locations/test_sftp.py
@@ -1,11 +1,13 @@
+import json
 import os
 import pathlib
 
 import pandas as pd
 import pytest
 
-from astro.constants import Database
+from astro.constants import Database, FileType
 from astro.files import File
+from astro.files.locations import create_file_location
 from astro.settings import SCHEMA
 from astro.table import Metadata, Table
 from astro.utils.load import copy_remote_file_to_local
@@ -74,4 +76,44 @@ def test_export_table_to_file_in_the_cloud(database_table_fixture):
         ]
     )
     test_utils.assert_dataframes_are_equal(df, expected)
+    os.remove(filepath)
+
+
+@pytest.mark.integration
+@pytest.mark.parametrize(
+    "path, filetype",
+    [
+        ("sftp://upload/filename", FileType.CSV),
+        ("sftp://upload/filename", FileType.JSON),
+        ("sftp://upload/filename", FileType.NDJSON),
+        ("sftp://upload/filename", FileType.PARQUET),
+    ],
+)
+def test_create_from_dataframe(path, filetype):
+    """Test get_size() of for SFTP file."""
+    data = {"id": [1, 2, 3], "name": ["First", "Second", "Third with unicode पांचाल"]}
+    df = pd.DataFrame(data=data)
+    path = path + "." + filetype.value
+    final_path = "sftp://foo:pass@localhost:2222/upload/filename." + filetype.value
+    location = create_file_location(path, DEFAULT_CONN_ID)
+    location.create_from_dataframe("upload/filename." + filetype.value, df, filetype)
+    filepath = copy_remote_file_to_local(
+        source_filepath=final_path, is_binary=True, transport_params={"connect_kwargs": {"password": "pass"}}
+    )
+    count = 0
+    if filetype == FileType.CSV:
+        df = pd.read_csv(filepath)
+        count = len(df)
+    elif filetype == FileType.JSON:
+        df = pd.read_json(filepath)
+        count = len(df)
+    elif filetype == FileType.NDJSON:
+        with open(filepath) as file:
+            for _, line in enumerate(file):
+                assert len(json.loads(line).keys()) == 2
+                count = count + 1
+    elif filetype == FileType.PARQUET:
+        df = pd.read_parquet(filepath)
+        count = len(df)
+    assert count == 3
     os.remove(filepath)
