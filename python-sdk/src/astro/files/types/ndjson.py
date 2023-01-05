6c45a2ca12399b99684fde188e1677fb28f19b39
Pankaj <pankaj.singh@astronomer.io>
2023-01-05 17:36:17 +0530

    Address review feedback

--- python-sdk/src/astro/files/types/ndjson.py
+++ python-sdk/src/astro/files/types/ndjson.py
@@ -29,7 +29,7 @@ class NDJSONFileType(FileType):
         :param columns_names_capitalization: determines whether to convert all columns to lowercase/uppercase
             in the resulting dataframe
         """
-        if load_options is not None:
+        if isinstance(load_options, PandasLoadOptions):
             kwargs.update(load_options.to_dict)
         df = NDJSONFileType.flatten(self.normalize_config, stream, **kwargs)
         df = convert_columns_names_capitalization(
