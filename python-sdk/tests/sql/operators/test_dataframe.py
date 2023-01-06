334a26d50c7660d316bac30c8234fa066b77d7be
Pankaj <pankaj.singh@astronomer.io>
2023-01-07 00:59:20 +0530

    Add tests

--- python-sdk/tests/sql/operators/test_dataframe.py
+++ python-sdk/tests/sql/operators/test_dataframe.py
@@ -13,6 +13,7 @@ import astro.sql as aql
 from astro.airflow.datasets import DATASET_SUPPORT
 from astro.files import File
 from astro.table import Table
+from astro.sql.operators.dataframe import DataframeOperator
 
 from ..operators import utils as test_utils
 
@@ -235,3 +236,17 @@ def test_dataframe_from_file_xcom_pickling(mock_serde, sample_dag):
     test_utils.run_dag(sample_dag)
     mock_serde.serialize.assert_not_called()
     mock_serde.deserialize.assert_not_called()
+
+
+@pytest.mark.parametrize(
+    "exception", [OSError("os error"), TypeError("type error")]
+)
+@mock.patch("astro.sql.operators.base_decorator.inspect.getsource", autospec=True)
+def test_get_source_code_handle_exception(mock_getsource, exception):
+    """assert get_source_code not raise exception"""
+    mock_getsource.side_effect = exception
+    DataframeOperator(
+        task_id="test",
+        sql="select * from 1",
+        python_callable=lambda: 1
+    ).get_source_code(py_callable=None)
