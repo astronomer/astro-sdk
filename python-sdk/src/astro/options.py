acd7ee4c979d7bd612c5849c313108efaf9f8735
Daniel Imberman <daniel.imberman@gmail.com>
2023-01-09 16:56:13 -0800

    Add a snowflake load options (#1516)
    # Description
    ## What is the current behavior?
    The current behavior for loading data to Snowflake is to depend on
    `native_kwargs`, which are loosely defined and lack type safety. There
    is also no way to specify `FILE_FORMAT` options in Snowflake, which can
    block users from being able to load their data.
    
    ## What is the new behavior?
    This PR introduces a `SnowflakeLoadOptions` class that contains
    `file_format_options` and `copy_options` fields. These fields allow
    users to specify the necessary arguments to correctly load their data.
    
    ## Does this introduce a breaking change?
    No, this does not introduce a breaking change.
    
    ### Checklist
    - [ ] Created tests which fail without the change (if possible)
    - [ ] Extended the README / documentation, if necessary

--- python-sdk/src/astro/options.py
+++ python-sdk/src/astro/options.py
@@ -5,3 +5,12 @@ import attr
 class LoadOptions:
     def empty(self):
         return NotImplementedError()
+
+
+@attr.define
+class SnowflakeLoadOptions(LoadOptions):
+    file_options: dict = attr.field(init=True, factory=dict)
+    copy_options: dict = attr.field(init=True, factory=dict)
+
+    def empty(self):
+        return not self.file_options and not self.copy_options
