912b07cfd5f8eeb8bea3b14803c235e118c1d7eb
Daniel Imberman <daniel.imberman@gmail.com>
2023-01-09 16:56:13 -0800

    Add a snowflake load options (#1516)
    # Description
    ## What is the current behavior?
    The current behavior for loading data to Snowflake is to depend on
    `native_kwargs`, which are loosely defined and lack type safety. There
    is also no way to specify `FILE_FORMAT` options in Snowflake, which can
    block users from being able to load their data.
    
    ## What is the new behavior?
    This PR introduces a `SnowflakeLoadOptions` class that contains
    `file_format_options` and `copy_options` fields. These fields allow
    users to specify the necessary arguments to correctly load their data.
    
    ## Does this introduce a breaking change?
    No, this does not introduce a breaking change.
    
    ### Checklist
    - [ ] Created tests which fail without the change (if possible)
    - [ ] Extended the README / documentation, if necessary

--- python-sdk/src/astro/databases/snowflake.py
+++ python-sdk/src/astro/databases/snowflake.py
@@ -38,7 +38,7 @@ from astro.constants import (
 from astro.databases.base import BaseDatabase
 from astro.exceptions import DatabaseCustomError
 from astro.files import File
-from astro.options import LoadOptions
+from astro.options import LoadOptions, SnowflakeLoadOptions
 from astro.settings import LOAD_TABLE_AUTODETECT_ROWS_COUNT, SNOWFLAKE_SCHEMA
 from astro.table import BaseTable, Metadata
 
@@ -368,6 +368,7 @@ class SnowflakeDatabase(BaseDatabase):
         file: File,
         storage_integration: str | None = None,
         metadata: Metadata | None = None,
+        load_options: SnowflakeLoadOptions | None = None,
     ) -> SnowflakeStage:
         """
         Creates a new named external stage to use for loading data from files into Snowflake
@@ -390,18 +391,24 @@ class SnowflakeDatabase(BaseDatabase):
         """
         auth = self._create_stage_auth_sub_statement(file=file, storage_integration=storage_integration)
 
+        if not load_options:
+            load_options = SnowflakeLoadOptions()
         metadata = metadata or self.default_metadata
         stage = SnowflakeStage(metadata=metadata)
         stage.set_url_from_file(file)
 
         fileformat = ASTRO_SDK_TO_SNOWFLAKE_FILE_FORMAT_MAP[file.type.name]
-        copy_options = COPY_OPTIONS[file.type.name]
-
+        copy_options = [COPY_OPTIONS[file.type.name]]
+        copy_options.extend([f"{k}={v}" for k, v in load_options.copy_options.items()])
+        file_options = [f"{k}={v}" for k, v in load_options.file_options.items()]
+        file_options.extend([f"TYPE={fileformat}", "TRIM_SPACE=TRUE"])
+        file_options_str = ", ".join(file_options)
+        copy_options_str = ", ".join(copy_options)
         sql_statement = "".join(
             [
                 f"CREATE OR REPLACE STAGE {stage.qualified_name} URL='{stage.url}' ",
-                f"FILE_FORMAT=(TYPE={fileformat}, TRIM_SPACE=TRUE) ",
-                f"COPY_OPTIONS=({copy_options}) ",
+                f"FILE_FORMAT=({file_options_str}) ",
+                f"COPY_OPTIONS=({copy_options_str}) ",
                 auth,
             ]
         )
@@ -577,6 +584,7 @@ class SnowflakeDatabase(BaseDatabase):
         target_table: BaseTable,
         if_exists: LoadExistStrategy = "replace",
         native_support_kwargs: dict | None = None,
+        load_options: LoadOptions | None = None,
         **kwargs,
     ):  # skipcq PYL-W0613
         """
@@ -591,6 +599,7 @@ class SnowflakeDatabase(BaseDatabase):
         retrieved from the Airflow connection or from the `storage_integration`
         attribute within `native_support_kwargs`.
 
+        :param load_options: Options for format and copy options when loading data to snowflake
         :param source_file: File from which we need to transfer data
         :param target_table: Table to which the content of the file will be loaded to
         :param if_exists: Strategy used to load (currently supported: "append" or "replace")
@@ -605,8 +614,20 @@ class SnowflakeDatabase(BaseDatabase):
         """
         native_support_kwargs = native_support_kwargs or {}
         storage_integration = native_support_kwargs.get("storage_integration")
-        stage = self.create_stage(file=source_file, storage_integration=storage_integration)
+        if not load_options:
+            load_options = SnowflakeLoadOptions()
+        if not isinstance(load_options, SnowflakeLoadOptions):
+            raise ValueError("Error: Requires a SnowflakeLoadOptions")
+        stage = self.create_stage(
+            file=source_file, storage_integration=storage_integration, load_options=load_options
+        )
 
+        rows = self._copy_into_table_from_stage(
+            source_file=source_file, target_table=target_table, stage=stage
+        )
+        self.evaluate_results(rows)
+
+    def _copy_into_table_from_stage(self, source_file, target_table, stage):
         table_name = self.get_table_qualified_name(target_table)
         file_path = os.path.basename(source_file.path) or ""
         sql_statement = f"COPY INTO {table_name} FROM @{stage.qualified_name}/{file_path}"
@@ -625,7 +646,7 @@ class SnowflakeDatabase(BaseDatabase):
             raise DatabaseCustomError from exe
         finally:
             self.drop_stage(stage)
-        self.evaluate_results(rows)
+        return rows
 
     @staticmethod
     def evaluate_results(rows):
