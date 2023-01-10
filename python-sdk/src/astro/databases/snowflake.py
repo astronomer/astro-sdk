7866de4f0a9d0e046e99d422adcf928df715099b
utkarsh sharma <utkarsharma2@gmail.com>
2023-01-10 18:01:53 +0530

    Fix CI

--- python-sdk/src/astro/databases/snowflake.py
+++ python-sdk/src/astro/databases/snowflake.py
@@ -263,7 +263,7 @@ class SnowflakeDatabase(BaseDatabase):
     ):
         super().__init__(conn_id)
         self.table = table
-        if not isinstance(load_options, SnowflakeLoadOptions):
+        if load_options is not None or not isinstance(load_options, SnowflakeLoadOptions):
             raise ValueError("Error: Requires a SnowflakeLoadOptions")
         self.load_options: SnowflakeLoadOptions | None = load_options
 
