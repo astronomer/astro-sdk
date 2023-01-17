dd80683b6a8ab6d5988c80ed11de6c499ccc91ae
Pankaj Singh <98807258+pankajastro@users.noreply.github.com>
2023-01-18 00:52:13 +0530

    Fix data validation tests (#1613)
    # Description
    ## What is the current behavior?
    currently tests are failing in main with error 
    ```
    FAILED tests_integration/sql/operators/data_validation/test_check_column.py::test_column_check_operator_with_table_dataset[snowflake] - AttributeError: module 'astro.sql' has no attribute 'ColumnCheckOperator'
    FAILED tests_integration/sql/operators/data_validation/test_check_column.py::test_column_check_operator_with_table_dataset[bigquery] - AttributeError: module 'astro.sql' has no attribute 'ColumnCheckOperator'
    FAILED tests_integration/sql/operators/data_validation/test_check_column.py::test_column_check_operator_with_table_dataset[postgresql] - AttributeError: module 'astro.sql' has no attribute 'ColumnCheckOperator'
    FAILED tests_integration/sql/operators/data_validation/test_check_column.py::test_column_check_operator_with_table_dataset[sqlite] - AttributeError: module 'astro.sql' has no attribute 'ColumnCheckOperator'
    FAILED tests_integration/sql/operators/data_validation/test_check_column.py::test_column_check_operator_with_table_dataset[redshift] - AttributeError: module 'astro.sql' has no attribute 'ColumnCheckOperator'
    FAILED tests_integration/sql/operators/data_validation/test_check_table.py::test_column_check_operator_with_table_dataset[snowflake] - AttributeError: module 'astro.sql' has no attribute 'SQLCheckOperator'
    FAILED tests_integration/sql/operators/data_validation/test_check_table.py::test_column_check_operator_with_table_dataset[bigquery] - AttributeError: module 'astro.sql' has no attribute 'SQLCheckOperator'
    FAILED tests_integration/sql/operators/data_validation/test_check_table.py::test_column_check_operator_with_table_dataset[postgresql] - AttributeError: module 'astro.sql' has no attribute 'SQLCheckOperator'
    FAILED tests_integration/sql/operators/data_validation/test_check_table.py::test_column_check_operator_with_table_dataset[sqlite] - AttributeError: module 'astro.sql' has no attribute 'SQLCheckOperator'
    FAILED tests_integration/sql/operators/data_validation/test_check_table.py::test_column_check_operator_with_table_dataset[redshift] - AttributeError: module 'astro.sql' has no attribute 'SQLCheckOperator'
    ```
    
    
    ## What is the new behavior?
    Fix import path
    
    ## Does this introduce a breaking change?
    
    
    ### Checklist
    - [ ] Created tests which fail without the change (if possible)
    - [ ] Extended the README / documentation, if necessary
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Daniel Imberman <daniel.imberman@gmail.com>

--- python-sdk/src/astro/sql/__init__.py
+++ python-sdk/src/astro/sql/__init__.py
@@ -4,6 +4,8 @@ from airflow.models.xcom_arg import XComArg
 
 from astro.sql.operators.append import AppendOperator, append
 from astro.sql.operators.cleanup import CleanupOperator, cleanup
+from astro.sql.operators.data_validations.check_column import ColumnCheckOperator, check_column
+from astro.sql.operators.data_validations.check_table import SQLCheckOperator, check_table
 from astro.sql.operators.dataframe import DataframeOperator, dataframe
 from astro.sql.operators.drop import DropTableOperator, drop_table
 from astro.sql.operators.export_file import ExportFileOperator, export_file
