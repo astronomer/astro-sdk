34d785c92c5354caeccdd2ace7a24aecf30aab55
utkarsh sharma <utkarsharma2@gmail.com>
2023-02-08 17:10:33 +0530

    Update doc string

--- python-sdk/src/astro/dataframes/load_options.py
+++ python-sdk/src/astro/dataframes/load_options.py
@@ -9,33 +9,29 @@ from astro.options import LoadOptions
 @attr.define
 class PandasLoadOptions(LoadOptions):
     """
-    Pandas load options while reading and loading csv file.
+    Pandas load options while reading and loading different files. Some common used params are added to the class for
+     other valid options ref below. They can be passed in kwargs param:
+     1. CSV file type - https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
+     2. NDJSON/JSON file type - https://pandas.pydata.org/docs/reference/api/pandas.read_json.html
+     3. Parquet file type - https://pandas.pydata.org/docs/reference/api/pandas.read_parquet.html
 
-    :param delimiter: valid file type[CSV] - Delimiter to use. Defaults to None
-    :param dtype: valid file type[CSV] - Data type for data or columns.
+    :param delimiter: Delimiter to use. Defaults to None
+    :param dtype: Data type for data or columns.
         E.g. ``{"a": np.float64, "b": np.int32, "c": "Int64"}`` Use str or
         object together with suitable na_values settings to preserve and not interpret dtype. If converters are
         specified, they will be applied INSTEAD of dtype conversion.
-    :param encoding: valid file type[JSON/NDJSON] - Encoding to use for UTF when reading/writing (ex. ‘utf-8’).
+    :param encoding: Encoding to use for UTF when reading/writing (ex. ‘utf-8’).
         List of Python standard encodings: https://docs.python.org/3/library/codecs.html#standard-encodings
-    :param normalize_sep: valid file type[Parquet] - separator used to normalize nested ndjson.
+    :param normalize_sep: separator used to normalize nested ndjson.
          ex - ``{"a": {"b":"c"}}`` will result in: ``column - "a_b"`` where ``ndjson_normalize_sep = "_"``
     :param columns: If not None, only these columns will be read from the file.
     """
 
-    # CSV
     delimiter: str | None = None
-    dtype: DtypeArg | None = None
-
-    # JSON
-    encoding: str | None = None
-
-    # NDJSON
     normalize_sep: str | None = None
-
-    # Parquet
     columns: list[str] | None = None
-
+    dtype: DtypeArg | None = None
+    encoding: str | None = None
     kwargs: dict = attr.field(init=True, factory=dict)
 
     def populate_kwargs(self, kwargs):
