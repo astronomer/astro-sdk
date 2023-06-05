915b3da7fdbfa691c45a7a91acd3a3f5c572aa90
utkarsh sharma <utkarsharma2@gmail.com>
2023-06-05 22:47:28 +0530

    Amother Fix

--- python-sdk/tests_integration/astro_deploy/master_dag.py
+++ python-sdk/tests_integration/astro_deploy/master_dag.py
@@ -149,7 +149,7 @@ def create_sftp_ftp_airflow_connection(task_instance: Any) -> None:
     sftp_conn = Connection(
         conn_id="sftp_default",
         conn_type="sftp",
-        host=task_instance.xcom_pull(key=INSTANCE_PUBLIC_IP, task_ids=["create_ec2_instance"])[0],
+        host=task_instance.xcom_pull(key=INSTANCE_PUBLIC_IP, task_ids=["create_ec2_instance"]),
         login=SFTP_USERNAME,
         password=SFTP_PASSWORD
     )  # create a connection object
@@ -157,7 +157,7 @@ def create_sftp_ftp_airflow_connection(task_instance: Any) -> None:
     ftp_conn = Connection(
         conn_id="ftp_default",
         conn_type="ftp",
-        host=task_instance.xcom_pull(key=INSTANCE_PUBLIC_IP, task_ids=["create_ec2_instance"])[0],
+        host=task_instance.xcom_pull(key=INSTANCE_PUBLIC_IP, task_ids=["create_ec2_instance"]),
         login=FTP_USERNAME,
         password=FTP_PASSWORD
     )  # create a connection object
@@ -182,7 +182,7 @@ def terminate_instance(task_instance: "TaskInstance") -> None:
     import boto3
 
     ec2 = boto3.client("ec2", **AWS_S3_CREDS)
-    ec2_instance_id_xcom = task_instance.xcom_pull(key=EC2_INSTANCE_ID_KEY, task_ids=["create_ec2_instance"])[
+    ec2_instance_id_xcom = task_instance.xcom_pull(key=EC2_INSTANCE_ID_KEY, task_ids=["start_sftp_ftp_services"])[
         0
     ]
     ec2.terminate_instances(
