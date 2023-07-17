732434c82e0f169aa9f11511c3836681f0d433d6
utkarsh sharma <utkarsharma2@gmail.com>
2023-07-17 16:18:29 +0530

    Remove CommonSQL provider package

---
+++ python-sdk/example_dags/test.py
@@ -0,0 +1,36 @@
+import os
+from astro.constants import FileType
+from astro.files import File
+from astro import sql as aql
+from astro.table import Table, Metadata
+from airflow import DAG
+from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
+
+from datetime import datetime, timedelta
+
+s3_bucket = os.getenv("AWS_S3_BUCKET_SWGOH")
+
+dag = DAG(
+    dag_id="swgoh_bigquery_to_s3_test_dag",
+    start_date=datetime(2023, 6, 9),
+    schedule_interval=None,
+)
+with dag:
+    stage = "dev"  # TODO: modify when in "prod"
+
+    # bigquery_hook = BigQueryHook(gcp_conn_id='GCP_SWGOH_CONN_ID')
+
+    aql.export_to_file(
+        task_id="get_data",
+        input_data=Table(
+            conn_id="bigquery_new",  # This is your Google Bigquery connection name configured in Airflow
+            name="imdb_movies",
+            metadata=Metadata(schema="astro"),
+        ),
+        output_file=File(
+            path=f"s3://tmp9/character_training.csv",
+            conn_id="aws_conn",  # This is you AWS S3 connection name configured in Airflow
+            filetype=FileType.CSV,
+        ),
+        if_exists="replace",
+    )
