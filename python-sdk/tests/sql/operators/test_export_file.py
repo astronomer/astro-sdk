0fdd84c59716d6419e74da30c8c5de6383266fd4
utkarsh sharma <utkarsharma2@gmail.com>
2023-08-04 17:12:22 +0530

    Uncomment test

--- python-sdk/tests/sql/operators/test_export_file.py
+++ python-sdk/tests/sql/operators/test_export_file.py
@@ -1,3 +1,4 @@
+import os
 import pathlib
 
 import pandas as pd
@@ -10,7 +11,7 @@ from astro.files import File
 
 # Import Operator
 from astro.sql import ExportFileOperator, ExportTableToFileOperator, export_file, export_table_to_file
-from astro.sql.operators.export_to_file import ExportToFileOperator
+from astro.sql.operators.export_to_file import ExportToFileOperator, export_to_file
 from astro.table import Table
 
 from ..operators import utils as test_utils
@@ -79,46 +80,46 @@ def test_save_returns_output_file(sample_dag, database_table_fixture):
     assert input_df.equals(output_df)
 
 
-# @pytest.mark.parametrize(
-#     "database_table_fixture",
-#     [
-#         {
-#             "database": Database.SQLITE,
-#             "file": File(path=str(CWD) + "/../../data/homes.csv"),
-#         },
-#     ],
-#     indirect=True,
-# )
-# def test_unique_task_id_for_same_path(
-#     sample_dag,
-#     database_table_fixture,
-# ):
-#     _, test_table = database_table_fixture
-#     file_name = f"{test_utils.get_table_name('output')}.csv"
-#     OUTPUT_FILE_PATH = str(CWD) + f"/../../data/{file_name}"
-#
-#     tasks = []
-#     with sample_dag:
-#         for i in range(4):
-#             params = {
-#                 "input_data": test_table,
-#                 "output_file": File(path=OUTPUT_FILE_PATH),
-#                 "if_exists": "replace",
-#             }
-#
-#             if i == 3:
-#                 params["task_id"] = "task_id"
-#             task = export_to_file(**params)
-#             tasks.append(task)
-#     test_utils.run_dag(sample_dag)
-#
-#     assert tasks[0].operator.task_id != tasks[1].operator.task_id
-#     assert tasks[0].operator.task_id == "export_to_file"
-#     assert tasks[1].operator.task_id == "export_to_file__1"
-#     assert tasks[2].operator.task_id == "export_to_file__2"
-#     assert tasks[3].operator.task_id == "task_id"
-#
-#     os.remove(OUTPUT_FILE_PATH)
+@pytest.mark.parametrize(
+    "database_table_fixture",
+    [
+        {
+            "database": Database.SQLITE,
+            "file": File(path=str(CWD) + "/../../data/homes.csv"),
+        },
+    ],
+    indirect=True,
+)
+def test_unique_task_id_for_same_path(
+    sample_dag,
+    database_table_fixture,
+):
+    _, test_table = database_table_fixture
+    file_name = f"{test_utils.get_table_name('output')}.csv"
+    OUTPUT_FILE_PATH = str(CWD) + f"/../../data/{file_name}"
+
+    tasks = []
+    with sample_dag:
+        for i in range(4):
+            params = {
+                "input_data": test_table,
+                "output_file": File(path=OUTPUT_FILE_PATH),
+                "if_exists": "replace",
+            }
+
+            if i == 3:
+                params["task_id"] = "task_id"
+            task = export_to_file(**params)
+            tasks.append(task)
+    test_utils.run_dag(sample_dag)
+
+    assert tasks[0].operator.task_id != tasks[1].operator.task_id
+    assert tasks[0].operator.task_id == "export_to_file"
+    assert tasks[1].operator.task_id == "export_to_file__1"
+    assert tasks[2].operator.task_id == "export_to_file__2"
+    assert tasks[3].operator.task_id == "task_id"
+
+    os.remove(OUTPUT_FILE_PATH)
 
 
 @pytest.mark.skipif(not DATASET_SUPPORT, reason="Inlets/Outlets will only be added for Airflow >= 2.4")
