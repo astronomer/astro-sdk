f1ab110e1aa872c58e32ffe1f83c26866b63a3fc
Pankaj <pankaj.singh@astronomer.io>
2023-08-11 03:14:28 +0530

    Reuse test

--- python-sdk/tests_integration/sql/operators/test_load_file.py
+++ python-sdk/tests_integration/sql/operators/test_load_file.py
@@ -834,7 +834,7 @@ def test_load_file_bigquery_error_out(sample_dag, database_table_fixture):
     indirect=True,
     ids=["snowflake", "bigquery", "postgresql", "sqlite", "redshift", "duckdb", "mysql"],
 )
-@pytest.mark.parametrize("file_type", ["parquet", "ndjson", "json", "csv", "xlsx"])
+@pytest.mark.parametrize("file_type", ["parquet", "ndjson", "json", "csv"])
 def test_load_file(sample_dag, database_table_fixture, file_type):
     db, test_table = database_table_fixture
 
@@ -875,7 +875,7 @@ def test_load_file(sample_dag, database_table_fixture, file_type):
     indirect=True,
     ids=["mssql"],
 )
-@pytest.mark.parametrize("file_type", ["parquet", "ndjson", "json", "csv"])
+@pytest.mark.parametrize("file_type", ["parquet", "ndjson", "json", "csv", "xlsx"])
 def test_load_file_for_mssql(sample_dag, database_table_fixture, file_type):
     """Test loading a file into MSSQL table and then exporting the table to dataframe works successfully"""
     db, test_table = database_table_fixture
