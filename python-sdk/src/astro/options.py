c4bccf818f934223201ed85c03ae7b91d0408fac
Pankaj Koti <pankaj.koti@astronomer.io>
2023-08-30 15:38:27 +0530

    Support loading metadata columns from stage into table for Snowflake (#2023)
    Adds support to load metadata columns like METADATA$FILENAME, 
    METADATA$FILE_ROW_NUMBER, etc. from stage into target table 
    while natively loading files into Snowflake tables. Read more at: 
    
    https://docs.snowflake.com/en/user-guide/querying-metadata#example-3-loading-metadata-columns-into-a-table
    
    Note that, you cannot specify both `validation_mode` and 
    `metadata_columns` together in the Snowflake load options 
    because when we need to load `metadata_columns`, we need 
    to explicitly name the metadata columns in the `COPY INTO` 
    sql statement and such a transformed SQL statement does not 
    allow specifying `VALIDATION_MODE` with it. It's a limitation 
    for Snowflake queries.
    The transformed SQL appears like in the snippet in the following link: 
    
    https://docs.snowflake.com/en/user-guide/querying-metadata#example-3-loading-metadata-columns-into-a-table
    
    closes: #1982
    
    ---------
    
    Co-authored-by: Utkarsh Sharma <utkarsharma2@gmail.com>

--- python-sdk/src/astro/options.py
+++ python-sdk/src/astro/options.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional
+from __future__ import annotations
 
 import attr
 
@@ -15,14 +15,14 @@ class LoadOptions:
         return attr.asdict(self)
 
 
-def contains_required_option(load_options: Optional[LoadOptions], option_name: str) -> bool:
+def contains_required_option(load_options: LoadOptions | None, option_name: str) -> bool:
     """
     Check required options in load_option class
     """
     return bool(load_options and getattr(load_options, option_name, None))
 
 
-def list_to_dict(value: Optional[List[LoadOptions]]) -> Optional[Dict[str, LoadOptions]]:
+def list_to_dict(value: list[LoadOptions] | None) -> dict[str, LoadOptions] | None:
     """
     Convert list object to dict
     """
@@ -33,9 +33,9 @@ def list_to_dict(value: Optional[List[LoadOptions]]) -> Optional[Dict[str, LoadO
 
 @attr.define
 class LoadOptionsList:
-    _load_options: Optional[Dict[str, LoadOptions]] = attr.field(converter=list_to_dict)
+    _load_options: dict[str, LoadOptions] | None = attr.field(converter=list_to_dict)
 
-    def get(self, option_class) -> Optional[LoadOptions]:
+    def get(self, option_class) -> LoadOptions | None:
         """
         Check `LOAD_OPTIONS_CLASS_NAME` attribute and select the correct load_options
         :param option_class: FileType | FileLocation | BaseDatabase
@@ -49,7 +49,7 @@ class LoadOptionsList:
                 break
         return cls
 
-    def get_by_class_name(self, option_class_name) -> Optional[LoadOptions]:
+    def get_by_class_name(self, option_class_name) -> LoadOptions | None:
         """
         Get load_option by class name
         :return:
@@ -64,11 +64,14 @@ class SnowflakeLoadOptions(LoadOptions):
     """
     Load options to load file to snowflake using native approach.
 
+    :param copy_options: Specify one or more of the copy option as key-value pair. Read more at:
+        https://docs.snowflake.com/en/sql-reference/sql/copy-into-table.html#copy-options-copyoptions
     :param file_options: Depending on the file format type specified, use one or more of the
         format-specific options as key-value pair. Read more at:
         https://docs.snowflake.com/en/sql-reference/sql/copy-into-table.html#format-type-options-formattypeoptions
-    :param copy_options: Specify one or more of the copy option as key-value pair. Read more at:
-        https://docs.snowflake.com/en/sql-reference/sql/copy-into-table.html#copy-options-copyoptions
+    :param metadata_columns: Specify one or more metadata columns to be included from Snowflake stage into the target
+        table. Read more at:
+        https://docs.snowflake.com/en/user-guide/querying-metadata#example-3-loading-metadata-columns-into-a-table
     :param storage_integration: Specify the previously created Snowflake storage integration
     :param validation_mode: Defaults to `validation_mode=None`. This instructs the COPY command to validate the data
         files instead of loading them into the specified table; i.e. the COPY command tests the files for errors but
@@ -86,12 +89,18 @@ class SnowflakeLoadOptions(LoadOptions):
           to `CONTINUE` during the load.
 
         Read more at: https://docs.snowflake.com/en/sql-reference/sql/copy-into-table.html#optional-parameters
+
+    .. note::
+        If you specify `validation_mode` and `metadata_columns` together, `metadata_columns` will not be loaded as the
+        transformed `COPY INTO` command fails with SQL compilation error, and the load file operation will fall back to
+        the non-native approach using pandas.
     """
 
-    file_options: dict = attr.field(init=True, factory=dict)
     copy_options: dict = attr.field(init=True, factory=dict)
-    validation_mode: str = attr.field(default=None)
+    file_options: dict = attr.field(init=True, factory=dict)
+    metadata_columns: list[str] = attr.field(default=None)
     storage_integration: str = attr.field(default=None)
+    validation_mode: str = attr.field(default=None)
 
     def empty(self):
         return not self.file_options and not self.copy_options
