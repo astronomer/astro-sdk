c8df0c610b1842fd9b6bf63345fd3cfe30e78f54
Pankaj Singh <98807258+pankajastro@users.noreply.github.com>
2024-01-25 20:12:17 +0530

    Use apache-airflow-providers-openlineage in debug log (#2114)
    since now import from `apache-airflow-providers-openlineage` so let's
    update the log accordingly

--- python-sdk/src/astro/lineage/__init__.py
+++ python-sdk/src/astro/lineage/__init__.py
@@ -1,6 +1,23 @@
 import logging
+import os
+
+from airflow.configuration import conf
 
 log = logging.getLogger(__name__)
+
+
+def _is_disabled() -> bool:
+    return (
+        conf.getboolean("openlineage", "disabled", fallback=False)
+        or os.getenv("OPENLINEAGE_DISABLED", "false").lower() == "true"
+        or (
+            conf.get("openlineage", "transport", fallback="") == ""
+            and conf.get("openlineage", "config_path", fallback="") == ""
+            and os.getenv("OPENLINEAGE_URL", "") == ""
+            and os.getenv("OPENLINEAGE_CONFIG", "") == ""
+        )
+    )
+
 
 try:
     from airflow.providers.openlineage.extractors import OperatorLineage
@@ -16,4 +33,5 @@ try:
     )
     from openlineage.client.run import Dataset as OpenlineageDataset
 except ImportError:
-    logging.debug("openlineage-airflow python dependency is missing")
+    if not _is_disabled():
+        logging.error("apache-airflow-providers-openlineage python dependency is missing")
